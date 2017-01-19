272ba8e4254040886dba8178ec437a095cd415c0
Ricardo Magalhães <magalhini@gmail.com>
2017-01-19 16:16:10 +0100

    Add dist/index

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Button = undefined;
+
+var _Button2 = require('./Button');
+
+var _Button3 = _interopRequireDefault(_Button2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Button = _Button3.default;
