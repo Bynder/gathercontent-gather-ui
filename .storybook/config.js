0e082214ca96bd9cec137d5754320fbf6b370e49
Ricardo Magalhães <magalhini@gmail.com>
2017-01-23 15:28:37 +0100

    Add file loaders and import FontAwesome from Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure } from '@kadira/storybook';
 
 require('../styles/bootstrap/bootstrap.scss');
+require('../styles/font-awesome/scss/font-awesome.scss');
 require('../styles/main.scss');
 
 function loadStories() {
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,6 +7,13 @@ module.exports = {
         test: /.scss$/,
         loaders: ['style', 'css', 'postcss-loader', 'sass'],
         include: path.resolve(__dirname, '../')
+      }, {
+        test: /\.svg$/,
+        loader: 'raw-loader'
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file?name=styles/font-awesome/fonts/[name].[ext]'
       }
     ]
   },
