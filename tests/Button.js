760ffc978422060b363322d6d831b9f0a83712eb
Kyle Shrives <kyle@gathercontent.com>
2017-02-17 15:12:07 +0000

    functional refactor for Button component

--- tests/Button.js
+++ tests/Button.js
@@ -1,84 +1,58 @@
-import { React, expect, sinon, jsDomGlobal, shallow, mount } from './setup';
+import { React, expect, sinon, jsDomGlobal, mount } from './setup';
 import Button from '../lib/Button';
 jsDomGlobal();
 
 describe('Button', () => {
-  let sandbox, props;
+  let sandbox, wrapper, button, clickHandlerSpy;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
+    clickHandlerSpy = sandbox.spy();
+    wrapper = mount(<Button clickHandler={clickHandlerSpy}>Botão</Button>);
+    button = wrapper.find('button');
   });
 
   afterEach(() => {
     sandbox.restore();
   });
 
-  it('should render a primary button component', () => {
-    props = { value: 'Botão', type: 'primary' };
-
-    let Element = shallow(<Button {...props}/>);
-    let button = Element.find('button');
-
-    expect(button).to.have.length(1);
-    expect(button.text()).to.equal('Botão');
-    expect(button.props().className).to.contain('primary');
+  it('should render text passed on children', () => {
+    expect(button.prop('children')).to.equal('Botão');
   });
 
-  it('should render a secondary button component', () => {
-    props = { type: 'secondary' };
-
-    let Element = shallow(<Button {...props}/>);
-
-    expect(Element
-      .find('button')
-      .props()
-      .className
-    ).to.equal('btn btn--secondary');
+  it('should render a primary button by default', () => {
+    expect(button.prop('className')).contains('button button--primary');
   });
 
-  it('should receive a custom class name when given one as props', () => {
-    props = {type: 'primary', className: 'custom--class'};
-
-    let Element = shallow(<Button {...props} />);
-
-    expect(Element
-      .find('button')
-      .props()
-      .className)
-      .to.equal('btn btn--primary custom--class');
+  it('should render a button with multiple type classes', () => {
+    wrapper.setProps({
+      'types': ['clear', 'collapsed'],
+    });
+    expect(button.prop('className')).contains('button button--clear button--collapsed');
   });
 
-  it('should call its onClick handler when clicked', () => {
-    props = {
-      value: 'Second',
-      type: 'secondary',
-      clickHandler: sandbox.spy()
-    };
-
-    let Element = mount(<Button {...props}/>);
-    let button = Element.find('button').simulate('click');
-
-    expect(props.clickHandler.called).to.equal(true);
+  it('should render custom classes', () => {
+    wrapper.setProps({
+      className: 'custom-class'
+    });
+    expect(button.prop('className')).contains('custom-class');
   });
 
-  it('should set state.disabled to true when clicked', () => {
-    props = {
+  it('should call props.clickHandler when clicked', () => {
+    wrapper.simulate('click');
+    expect(clickHandlerSpy.calledOnce).to.equal(true);
+  });
+
+  it('should set state.disabled to true when clicked if props.disableOnClick is true', () => {
+    wrapper.setProps({
       disableOnClick: true,
-      clickHandler: sandbox.spy()
-    };
-
-    let Element = shallow(<Button {...props}/>);
-    let button = Element.find('button').simulate('click');
-    expect(Element.state('disabled')).to.equal(true);
+    });
+    wrapper.simulate('click');
+    expect(wrapper.state('disabled')).to.equal(true);
   });
 
-  it('should not set state.disabled to true when clicked', () => {
-    props = {
-      clickHandler: sandbox.spy()
-    };
-
-    let Element = shallow(<Button {...props}/>);
-    let button = Element.find('button').simulate('click');
-    expect(Element.state('disabled')).to.equal(false);
+  it('should not set state.disabled to true when clicked if props.disableOnClick is false', () => {
+    wrapper.simulate('click');
+    expect(wrapper.state('disabled')).to.equal(false);
   });
 });
