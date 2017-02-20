36ffd55516ebe4bcfb0e25f7ecf405e843a7f841
Kyle Shrives <kyle@gathercontent.com>
2017-02-20 12:30:10 +0000

    Changed NotificationAlert to Notification

--- stories/components/Notification.js
+++ stories/components/Notification.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
-import NotificationAlert from '../../lib/NotificationAlert/';
+import Notification from '../../lib/Notification';
 import StoryItem from '../styleguide/StoryItem';
 
 storiesOf('Components', module)
@@ -9,19 +9,19 @@ storiesOf('Components', module)
       <StoryItem
         title="Notification Warning"
         description="A notification box with the warning level.">
-        <NotificationAlert level="warning" text="Warning, I am too sexy for this notification." />
+        <Notification level="warning" text="Warning, I am too sexy for this notification." />
       </StoryItem>
 
       <StoryItem
         title="Notification Danger"
         description="A notification box with the danger level.">
-        <NotificationAlert level="danger" text="You're about to delete your whole life." />
+        <Notification level="danger" text="You're about to delete your whole life." />
       </StoryItem>
 
       <StoryItem
         title="Notification Information"
         description="A notification box with the information level.">
-        <NotificationAlert level="info" text="Did you know Nirvana started in Aberdeen?" />
+        <Notification level="info" text="Did you know Nirvana started in Aberdeen?" />
       </StoryItem>
     </div>
   ));
