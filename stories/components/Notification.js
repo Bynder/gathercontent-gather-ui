9025abb40513305fc5070ac84da0cf64d864710e
Kyle Shrives <kyle@gathercontent.com>
2017-02-20 14:45:44 +0000

    Completed tests and fix other instances of mistyped instances of NotificationAlert.

--- stories/components/Notification.js
+++ stories/components/Notification.js
@@ -9,19 +9,25 @@ storiesOf('Components', module)
       <StoryItem
         title="Notification Warning"
         description="A notification box with the warning level.">
-        <Notification level="warning" text="Warning, I am too sexy for this notification." />
+        <Notification level="warning">Warning, I am too sexy for this notification.</Notification>
       </StoryItem>
 
       <StoryItem
         title="Notification Danger"
         description="A notification box with the danger level.">
-        <Notification level="danger" text="You're about to delete your whole life." />
+        <Notification level="danger">You're about to delete your whole life.</Notification>
       </StoryItem>
 
       <StoryItem
         title="Notification Information"
         description="A notification box with the information level.">
-        <Notification level="info" text="Did you know Nirvana started in Aberdeen?" />
+        <Notification level="info">Did you know <strong>Nirvana</strong> started in <u>Aberdeen</u>?</Notification>
+      </StoryItem>
+
+      <StoryItem
+        title="Notification (with a click handler)"
+        description="A notification can handle click interactions.">
+        <Notification level="warning" clickHandler={action('clickHandler')}><strong>You have overdue projects.</strong></Notification>
       </StoryItem>
     </div>
   ));
