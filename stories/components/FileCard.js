520f4462f149b430bbed084f7f8976740ebaeb7d
Ricardo Magalhães <magalhini@gmail.com>
2017-05-31 12:23:45 +0100

    First styling iteration for file card thumbnails

--- stories/components/FileCard.js
+++ stories/components/FileCard.js
@@ -9,9 +9,26 @@ storiesOf('Components', module)
       <div>
         <StoryItem
           title="File Card"
-          description="...">
-          <FileCard />
+          description="A single image thumbnail">
+          <FileCard
+            type="image"
+            filename="sheep_in_iceland.jpg"
+            label="Sheep in Iceland"
+            image="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+          />
         </StoryItem>
+
+        <StoryItem
+        title="File Card"
+        description="A single image thumbnail which has comments.">
+        <FileCard
+          hasComments
+          type="image"
+          filename="sheep_in_iceland.jpg"
+          label="Sheep in Iceland"
+          image="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+        />
+      </StoryItem>
       </div>
     );
   });
