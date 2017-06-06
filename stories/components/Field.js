cfb920dbe3e6ea92297c4e9fef04326e6e336470
Kyle Shrives <kyle@gathercontent.com>
2017-06-06 15:18:06 +0100

    Added prop types and layout components to Field

--- stories/components/Field.js
+++ stories/components/Field.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf, action } from '@storybook/react';
-import Field from '../../lib/Field/';
+import Field from '../../lib/Field';
 import StoryItem from '../styleguide/StoryItem';
 
 const mockValidations = [{
@@ -26,21 +26,14 @@ storiesOf('Components', module)
         <StoryItem
           title="Field"
           description="A field component">
-          <div className="container">
-            <div className="grid">
-              <div className="grid__cell grid__cell--1-2 grid__cell--offset-1-4">
-                <Field
-                  label="Test Label Text"
-                  actions={mockActions}
-                  validations={mockValidations}
-                  guidelines="Guideline text here!!!"
-                >
-                  <div>Add editor component here from app!</div>
-                </Field>
-              </div>
-              <div className="grid__cell grid__cell--1-4">Comments...</div>
-            </div>
-          </div>
+          <Field
+            label="Test Label Text"
+            actions={mockActions}
+            validations={mockValidations}
+            guidelines="Guideline text here!!!"
+          >
+            <div>Add editor component here from app!</div>
+          </Field>
         </StoryItem>
 
         <StoryItem
