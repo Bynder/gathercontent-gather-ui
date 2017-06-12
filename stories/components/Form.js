1883127a36e8f4774876fd7a527b4a903d35b176
Ricardo Magalhães <magalhini@gmail.com>
2017-06-12 14:30:21 +0200

    Checkboxes and Choices can now be disabled

--- stories/components/Form.js
+++ stories/components/Form.js
@@ -39,6 +39,31 @@ storiesOf('Components', module)
             <Checkbox.Label label="Honduras roast" id="id3" />
           </p>
         </StoryItem>
+
+        <StoryItem
+          title="Checkboxes: Disabled"
+          description="Radio input choices can be disabled. This can be achieved by passing in the disabled property."
+        >
+          <p>
+            <Checkbox.Input
+              name="ch44"
+              id="id44"
+              disabled
+              onChangeHandler={action('hello')}
+              checked
+            />
+            <Checkbox.Label label="Ethiopian roast" id="id44" />
+          </p>
+          <p>
+            <Checkbox.Input
+              name="ch44"
+              disabled
+              onChangeHandler={action('hello')}
+              id="id55"
+            />
+            <Checkbox.Label label="Guatemala roast" id="id55" />
+          </p>
+        </StoryItem>
       </div>
     )
   })
@@ -77,6 +102,33 @@ storiesOf('Components', module)
               onChangeHandler={action('hello')}
             />
             <RadioButton.Label label="Honduras roast" id="id3" />
+          </p>
+        </StoryItem>
+
+        <StoryItem
+          title="Radio inputs: disabled"
+          description="Multiple radios can be disabled by passing in the disabled property"
+        >
+          <p>
+            <RadioButton.Input
+              name="ch21"
+              disabled
+              id="id21"
+              value="value 1"
+              onChangeHandler={action('hello')}
+              checked
+            />
+            <RadioButton.Label label="Ethiopian roast" id="id21" />
+          </p>
+          <p>
+            <RadioButton.Input
+              disabled
+              name="ch21"
+              value="value 2"
+              onChangeHandler={action('hello')}
+              id="id22"
+            />
+            <RadioButton.Label label="Guatemala roast" id="id22" />
           </p>
         </StoryItem>
       </div>
