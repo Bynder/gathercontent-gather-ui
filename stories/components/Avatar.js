4bb99b65ee735fc6860e392c73826c284d11cec5
Ricardo Magalhães <magalhini@gmail.com>
2017-07-03 14:10:19 +0200

    WIP: Avatar/Presence component updates on styling and tooltip properties

--- stories/components/Avatar.js
+++ stories/components/Avatar.js
@@ -9,42 +9,58 @@ storiesOf('Components', module)
     return (
       <div>
         <StoryItem
-          title="Avatar"
-          description="...">
+          title="Avatar — only with initials"
+          description="Simple avatars, to be used individually (not in a group). Here, examples with two colours passed through the component, with one assigned user (hover to see the tooltip).">
             <div>
               <AvatarPresence
-              url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
-              onlyInitials
-              initials="RF"
-              label="Seymour Butts"
+                url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
+                onlyInitials
+                initials="SB"
+                name="Seymour Butts"
+                email="example@gmail.com"
             />
 
             <AvatarPresence
-              initials="MM"
+              initials="PC"
               url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
               onlyInitials
               colour="rgb(230, 133, 173)"
-              label="Seymour Butts"
+              name="Poppy Cox"
+              email="poppycox@gmail.com"
+              isAssigned
             />
             </div>
         </StoryItem>
 
         <StoryItem
-          title="Avatar"
-          description="...">
+          title="Avatar — with image"
+          description="Without the `onlyInitials` prop and given a URL, it will display the image URL as the background.">
           <AvatarPresence
             url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
-            label="Seymour Butts"
+            name="Seymour Butts"
+            email="example@gmail.com"
+          />
+        </StoryItem>
+
+        <StoryItem
+          title="Avatar — faded out (signed off)"
+          description="Without the `onlyInitials` prop and given a URL, it will display the image URL as the background.">
+          <AvatarPresence
+            onlyInitials
+            name="Seymour Butts"
+            initials="SB"
+            email="example@gmail.com"
+            fadedOut
           />
         </StoryItem>
 
          <StoryItem
           title="Avatar List Group"
-          description="...">
+          description="A list of overlapping avatars can be used by wrapping them in a `AvatarPresenceGroup` component.">
             <AvatarPresenceGroup>
-              <AvatarPresence onlyInitials colour="rgb(249,95,93)" initials="TR" label="Seymour Butts" />
-              <AvatarPresence onlyInitials initials="BA" label="Seymour Butts" />
-              <AvatarPresence onlyInitials colour="rgb(249,95,93)" initials="LA" label="Seymour Butts" />
+              <AvatarPresence email="poppycox@gmail.com" onlyInitials isAssigned colour="rgb(249,95,93)" initials="MR" name="Mike Rotch" />
+              <AvatarPresence email="hugh@gmail.com" onlyInitials initials="HJ" name="Hugh Jass" />
+              <AvatarPresence email="la@gmail.com" onlyInitials colour="rgb(249,95,93)" initials="LA" name="Seymour Butts" />
             </AvatarPresenceGroup>
         </StoryItem>
       </div>
