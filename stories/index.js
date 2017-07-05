8829e87f3e524d651b3ff2b4f055b5a4f83c9a54
Ricardo Magalhães <magalhini@gmail.com>
2017-07-05 15:28:48 +0200

    Add Tooltip Story to the entry

--- stories/index.js
+++ stories/index.js
@@ -21,3 +21,4 @@ import Field from './components/Field';
 import FileCard from './components/FileCard';
 import Conversation from './components/Conversation';
 import Form from './components/Form';
+import Tooltip from './components/Tooltip';
