06e536c4faa54256e69ee6fadf0beae0eec0b0d4
Ricardo Magalhães <magalhini@gmail.com>
2017-07-07 16:29:00 +0200

    Fixed Avatar tests

--- tests/Avatar/AvatarPresence.js
+++ tests/Avatar/AvatarPresence.js
@@ -1,6 +1,6 @@
 import { React, expect, sinon, mount } from '../setup';
-import AvatarPresence from '../../lib/AvatarPresence';
-import AvatarPresenceGroup from '../../lib/AvatarPresence/AvatarGroup';
+import Avatar from '../../lib/Avatar';
+import AvatarPresenceGroup from '../../lib/AvatarGroup';
 
 describe('Avatar/Presence', () => {
   let wrapper;
@@ -10,10 +10,10 @@ describe('Avatar/Presence', () => {
     showMoreSpy = sinon.spy(AvatarPresenceGroup.prototype, 'showMore');
     wrapper = mount(
       <AvatarPresenceGroup maximum={3}>
-        <AvatarPresence email="poppycox@gmail.com" onlyInitials isAssigned fadedOut initials="MR" name="Mike Rotch" />
-        <AvatarPresence email="hugh@gmail.com" onlyInitials fadedOut initials="HJ" name="Hugh Jass" />
-        <AvatarPresence email="la@gmail.com" onlyInitials fadedOut initials="FD" name="Fedra Droid" />
-        <AvatarPresence email="la@gmail.com" onlyInitials fadedOut initials="KM" name="Kann Schemll" />
+        <Avatar email="poppycox@gmail.com" onlyInitials isAssigned fadedOut initials="MR" name="Mike Rotch" />
+        <Avatar email="hugh@gmail.com" onlyInitials fadedOut initials="HJ" name="Hugh Jass" />
+        <Avatar email="la@gmail.com" onlyInitials fadedOut initials="FD" name="Fedra Droid" />
+        <Avatar email="la@gmail.com" onlyInitials fadedOut initials="KM" name="Kann Schemll" />
     </AvatarPresenceGroup>
     );
   });
@@ -27,19 +27,19 @@ describe('Avatar/Presence', () => {
   });
 
   it('renders the maximum specified size of individual Avatar components', () => {
-    expect(wrapper.find(AvatarPresence).length).to.equal(3);
+    expect(wrapper.find(Avatar).length).to.equal(3);
   });
 
   it('renders a custom maximum specified size of individual Avatar components', () => {
     const customWrapper = mount(
       <AvatarPresenceGroup maximum={1}>
-        <AvatarPresence email="poppycox@gmail.com" onlyInitials isAssigned fadedOut initials="MR" name="Mike Rotch" />
-        <AvatarPresence email="hugh@gmail.com" onlyInitials fadedOut initials="HJ" name="Hugh Jass" />
-        <AvatarPresence email="la@gmail.com" onlyInitials fadedOut initials="FD" name="Fedra Droid" />
-        <AvatarPresence email="la@gmail.com" onlyInitials fadedOut initials="KM" name="Kann Schemll" />
+        <Avatar email="poppycox@gmail.com" onlyInitials isAssigned fadedOut initials="MR" name="Mike Rotch" />
+        <Avatar email="hugh@gmail.com" onlyInitials fadedOut initials="HJ" name="Hugh Jass" />
+        <Avatar email="la@gmail.com" onlyInitials fadedOut initials="FD" name="Fedra Droid" />
+        <Avatar email="la@gmail.com" onlyInitials fadedOut initials="KM" name="Kann Schemll" />
     </AvatarPresenceGroup>);
 
-    expect(customWrapper.find(AvatarPresence).length).to.equal(1);
+    expect(customWrapper.find(Avatar).length).to.equal(1);
   });
 
   it('renders an extra component displaying the amount of people not shown', () => {
@@ -56,7 +56,7 @@ describe('Avatar/Presence', () => {
   });
 
   it('renders the initials for an Avatar when the prop is enabled', () => {
-    const first = wrapper.find(AvatarPresence).first();
+    const first = wrapper.find(Avatar).first();
     expect(first.find('.avatar__initials').length).to.equal(1);
     expect(first.find('.avatar__initials').text()).to.equal('MR');
   });
