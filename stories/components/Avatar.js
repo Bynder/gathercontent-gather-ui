6e0a1ed151e37aa1594dab43a52f90ea5c3b014b
Ricardo Magalhães <magalhini@gmail.com>
2017-07-07 16:18:50 +0200

    Refactor AvatarPresence into separate Avatar and AvatarWithTooltip components

--- stories/components/Avatar.js
+++ stories/components/Avatar.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { storiesOf, action } from '@storybook/react';
-import AvatarPresence from '../../lib/AvatarPresence/';
-import AvatarPresenceGroup from '../../lib/AvatarPresence/AvatarGroup';
+import Avatar from '../../lib/Avatar/';
+import AvatarWithTooltip from '../../lib/Avatar/AvatarWithTooltip';
+import AvatarGroup from '../../lib/AvatarGroup';
 import StoryItem from '../styleguide/StoryItem';
 
 storiesOf('Components', module)
@@ -12,7 +13,7 @@ storiesOf('Components', module)
           title="Avatar — only with initials"
           description="Simple avatars, to be used individually (not in a group). Here, examples with two colours passed through the component, with one assigned user (hover to see the tooltip).">
             <div>
-              <AvatarPresence
+              <Avatar
                 url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
                 onlyInitials
                 initials="SB"
@@ -20,9 +21,8 @@ storiesOf('Components', module)
                 email="example@gmail.com"
             />
 
-            <AvatarPresence
+            <Avatar
               initials="PC"
-              url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
               onlyInitials
               colour="rgb(230, 133, 173)"
               name="Poppy Cox"
@@ -32,11 +32,10 @@ storiesOf('Components', module)
             </div>
         </StoryItem>
 
-         <StoryItem
-          title="Avatar — with image and no tooltip"
-          description="Passing a `noTooltip` property displays no tooltip on hover.">
-          <AvatarPresence
-            noTooltip
+        <StoryItem
+          title="Avatar — with image"
+          description="Passing a `url` property displays the avatar image.">
+          <Avatar
             url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
             name="Seymour Butts"
             email="example@gmail.com"
@@ -44,19 +43,30 @@ storiesOf('Components', module)
         </StoryItem>
 
         <StoryItem
-          title="Avatar — with image"
-          description="Without the `onlyInitials` prop and given a URL, it will display the image URL as the background.">
-          <AvatarPresence
+          title="Avatar — with image and tooltip"
+          description="We can use the `AvatarWithTooltip` component to have a tooltip displayed on the avatar.">
+          <AvatarWithTooltip
             url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
             name="Seymour Butts"
             email="example@gmail.com"
+          />
+        </StoryItem>
+
+        <StoryItem
+          title="Avatar — with image and tooltip"
+          description="Without the `onlyInitials` prop and given a URL, it will display the image URL as the background.">
+          <AvatarWithTooltip
+            url="https://pbs.twimg.com/profile_images/766954609306927104/ZHAfr9OP_400x400.jpg"
+            name="Seymour Butts"
+            email="example@gmail.com"
+            isAssigned
           />
         </StoryItem>
 
         <StoryItem
           title="Avatar — faded out (signed off)"
           description="Without the `onlyInitials` prop and given a URL, it will display the image URL as the background.">
-          <AvatarPresence
+          <Avatar
             onlyInitials
             name="Seymour Butts"
             initials="SB"
@@ -68,22 +78,22 @@ storiesOf('Components', module)
          <StoryItem
           title="Avatar List Group"
           description="A list of overlapping avatars can be used by wrapping them in a `AvatarPresenceGroup` component.">
-            <AvatarPresenceGroup>
-              <AvatarPresence email="poppycox@gmail.com" onlyInitials isAssigned colour="rgb(249,95,93)" initials="MR" name="Mike Rotch" />
-              <AvatarPresence email="hugh@gmail.com" onlyInitials initials="HJ" name="Hugh Jass" />
-              <AvatarPresence email="la@gmail.com" onlyInitials colour="rgb(249,95,93)" initials="LA" name="Seymour Butts" />
-            </AvatarPresenceGroup>
+            <AvatarGroup>
+              <Avatar email="poppycox@gmail.com" onlyInitials isAssigned colour="rgb(249,95,93)" initials="MR" name="Mike Rotch" />
+              <Avatar email="hugh@gmail.com" onlyInitials initials="HJ" name="Hugh Jass" />
+              <Avatar email="la@gmail.com" onlyInitials colour="rgb(249,95,93)" initials="LA" name="Seymour Butts" />
+            </AvatarGroup>
         </StoryItem>
 
         <StoryItem
           title="Avatar List Group — extra users"
-          description="A list of overlapping avatars can be used by wrapping them in a `AvatarPresenceGroup` component, only fitting in a maximum number of N. Other avatars can be displayed by tapping the plus sign.">
-            <AvatarPresenceGroup maximum={1}>
-              <AvatarPresence email="poppycox@gmail.com" onlyInitials isAssigned fadedOut initials="MR" name="Mike Rotch" />
-              <AvatarPresence email="hugh@gmail.com" onlyInitials fadedOut initials="HJ" name="Hugh Jass" />
-              <AvatarPresence email="la@gmail.com" onlyInitials fadedOut initials="FD" name="Fedra Droid" />
-              <AvatarPresence email="la@gmail.com" onlyInitials fadedOut initials="KM" name="Kann Schemll" />
-            </AvatarPresenceGroup>
+          description="A list of overlapping avatars can be used by wrapping them in a `AvatarGroup` component, only fitting in a maximum number of N. Other avatars can be displayed by tapping the plus sign.">
+            <AvatarGroup maximum={1}>
+              <Avatar email="poppycox@gmail.com" onlyInitials isAssigned fadedOut initials="MR" name="Mike Rotch" />
+              <Avatar email="hugh@gmail.com" onlyInitials fadedOut initials="HJ" name="Hugh Jass" />
+              <Avatar email="la@gmail.com" onlyInitials fadedOut initials="FD" name="Fedra Droid" />
+              <Avatar email="la@gmail.com" onlyInitials fadedOut initials="KM" name="Kann Schemll" />
+            </AvatarGroup>
         </StoryItem>
       </div>
     );
