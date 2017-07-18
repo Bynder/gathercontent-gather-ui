4d4de666bb3c748d61d440c11c123226d844cfb3
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2017-07-18 13:03:59 +0100

    Add some styling changes to the StatusIndicator

--- stories/components/StatusIndicator.js
+++ stories/components/StatusIndicator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf, action } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
 import StatusIndicator from '../../lib/StatusIndicator/';
 import StoryItem from '../styleguide/StoryItem';
 
@@ -7,12 +7,37 @@ storiesOf('Components', module)
   .add('StatusIndicator', () => {
     return (
       <div>
-        <StoryItem
-          title="Status Indicator"
-          description="Consists of a color and a label to express a status.">
-          <StatusIndicator color="#ff6300" label="Draft">
-            due <span>Tommorrow at 12:00pm</span>
+        <StoryItem title="Status Indicator" description="Current">
+          <StatusIndicator
+            color="rgb(250, 167, 50)"
+            label="Editing"
+            current
+            description={'Edits to be made in response to user testing and the feedback from the senior editor and subject matter expert.'}
+          >
+            <span className={'due-date'}>due Tommorrow at 12:00pm</span>
           </StatusIndicator>
+        </StoryItem>
+
+        <StoryItem description="Completed">
+          <StatusIndicator color="#3d8aeb" label="Review" completed>
+            <span className={'due-date'}>due Tommorrow at 12:00pm</span>
+          </StatusIndicator>
+        </StoryItem>
+
+        <StoryItem description="Consists of a color and a label to express a status.">
+          <StatusIndicator color="#ff6300" label="Draft">
+            <span className={'due-date'}>due Tommorrow at 12:00pm</span>
+          </StatusIndicator>
+        </StoryItem>
+
+        <StoryItem description="">
+          <StatusIndicator color="#93724f" label="Publishing">
+            <span className={'due-date'}>set a due date</span>
+          </StatusIndicator>
+        </StoryItem>
+
+        <StoryItem description="Optional children">
+          <StatusIndicator color="#965de8" label="Approval" />
         </StoryItem>
       </div>
     );
