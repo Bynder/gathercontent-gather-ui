e8645c912289720109be48b4bdd596dc510228eb
Kyle Shrives <kyle@gathercontent.com>
2017-09-08 10:20:06 +0100

    added primary and netural colours

--- stories/base/ColourPalette.js
+++ stories/base/ColourPalette.js
@@ -5,8 +5,17 @@ const ColourPalette = props => (
   <div className="sg__base">
     { props.colours.map(colour => (
       <div className="sg__colour">
-        <div style={{ backgroundColor: colour.value }} className="sg__colour-block" />
-        <p>{colour.name} <br /> {colour.value}</p>
+        <div
+          style={{ backgroundColor: colour.rgbValue }}
+          className="sg__colour-block"
+        />
+        <code className="sg__colour-var">{colour.name}</code>
+        <p className="sg__colour-value sg__colour-value--caps">
+          {colour.hexValue}
+        </p>
+        <p className="sg__colour-value">
+          {colour.rgbValue}
+        </p>
       </div>
     )) }
   </div>
