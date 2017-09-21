3582c8b006a2ebe4aacc7c971cf15b946296937a
Kyle Shrives <kyle@gathercontent.com>
2017-09-21 14:45:14 +0100

    Added new guideline component

---
+++ tests/Guideline/Guideline.js
@@ -0,0 +1,33 @@
+import { React, expect, shallow } from '../setup';
+import { Guideline, Button } from '../../lib';
+
+describe('Guideline', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Guideline title="Guideline title">
+        <p>Guideline content</p>
+      </Guideline>,
+    );
+  });
+
+  it('does not render its children', () => {
+    expect(wrapper.find('p')).to.have.length(0);
+  });
+
+  it('renders its children', () => {
+    wrapper.setState({ showContent: true });
+  });
+
+  it('sets the showContent state to true', () => {
+    wrapper.find(Button).prop('clickHandler')();
+    expect(wrapper.find('p')).to.have.length(1);
+  });
+
+  it('switches the text for the show toggle', () => {
+    expect(wrapper.find(Button).prop('children')).to.equal('Show details');
+    wrapper.setState({ showContent: true });
+    expect(wrapper.find(Button).prop('children')).to.equal('Hide details');
+  });
+});
