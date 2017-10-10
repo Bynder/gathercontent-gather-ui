c8ee2783d268f1f14131fa273388dfaa178a6616
Kyle Shrives <kyle@gathercontent.com>
2017-10-10 21:47:21 +0100

    Added border radius to wrapper and also made it so there are equal 3up sqaure for filecards

--- stories/components/FileCard.js
+++ stories/components/FileCard.js
@@ -9,69 +9,76 @@ const fullScreenButton = <button onClick={action('test')}><Icon name="fullScreen
 const downloadButton = <button onClick={action('test')}>{<Icon name="download" />}</button>;
 const actions = [trashButton, commentButton, fullScreenButton, downloadButton];
 
-storiesOf('Components', module)
-  .add('FileCard', () => (
-    <div>
-      <StoryItem
-        title="File Card"
-        description="A file card showing a preview of the file."
+storiesOf('Components', module).add('FileCard', () => (
+  <div>
+    <StoryItem
+      title="File Card"
+      description="A file card showing a preview of the file."
+    >
+      <FileCard
+        filename="cute_sheep_in_iceland.jpg"
+        label="Sheep in Iceland"
+        previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
       >
-        <FileCard
-          filename="cute_sheep_in_iceland.jpg"
-          label="Sheep in Iceland"
-          previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
-        >
-          {actions}
-        </FileCard>
-      </StoryItem>
+        {actions}
+      </FileCard>
+    </StoryItem>
 
-      <StoryItem
-        title="File Card (highlighted)"
-        description="A file card can have a highlighted state."
+    <StoryItem
+      title="File Card (highlighted)"
+      description="A file card can have a highlighted state."
+    >
+      <FileCard
+        filename="sheep_in_iceland.jpg"
+        label="Sheep in Iceland"
+        previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+        isHighlighted
       >
-        <FileCard
-          filename="sheep_in_iceland.jpg"
-          label="Sheep in Iceland"
-          previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
-          isHighlighted
-        >
-          {actions}
-        </FileCard>
-      </StoryItem>
+        {actions}
+      </FileCard>
+    </StoryItem>
 
-      <StoryItem
-        title="File Card (without preview)"
-        description="A file card without a preview image"
+    <StoryItem
+      title="File Card (without preview)"
+      description="A file card without a preview image"
+    >
+      <FileCard
+        filename="field_notes.txt"
+        label="Field notes"
       >
-        <FileCard
-          filename="field_notes.txt"
-          label="Field notes"
-        >
-          {actions}
-        </FileCard>
-      </StoryItem>
+        {actions}
+      </FileCard>
+    </StoryItem>
 
-      <StoryItem
-        title="Multiple File Cards"
-        description="Multiple file cards side by side"
+    <StoryItem
+      title="Multiple File Cards"
+      description="Multiple file cards side by side"
+    >
+      <FileCard
+        filename="sheep_in_iceland.jpg"
+        label="Sheep in Iceland"
+        previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+        isHighlighted
       >
-        <FileCard
-          filename="sheep_in_iceland.jpg"
-          label="Sheep in Iceland"
-          previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
-          isHighlighted
-        >
-          {actions}
-        </FileCard>
-        <FileCard
-          type="image"
-          filename="sunset_in_berlin_large_filename.jpg"
-          label="Large sunset in Berlin, Germany"
-          previewSrc="http://www.sdpsnet.org/sdps/images/conference/2012/hotel/mod_galleries_53.jpeg"
-          isHighlighted
-        >
-          {actions}
-        </FileCard>
-      </StoryItem>
-    </div>
-  ));
+        {actions}
+      </FileCard>
+      <FileCard
+        type="image"
+        filename="sunset_in_berlin_large_filename.jpg"
+        label="Large sunset in Berlin, Germany"
+        previewSrc="http://www.sdpsnet.org/sdps/images/conference/2012/hotel/mod_galleries_53.jpeg"
+        isHighlighted
+      >
+        {actions}
+      </FileCard>
+      <FileCard
+        filename="sheep_in_iceland.jpg"
+        label="Sheep in Iceland"
+        previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+        isHighlighted
+      >
+        {actions}
+      </FileCard>
+    </StoryItem>
+  </div>
+));
