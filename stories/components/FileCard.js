b7e16b07ba46059edf4c21d37ca63f82c7031eeb
Kyle Shrives <kyle@gathercontent.com>
2017-10-11 10:56:33 +0100

    cleaned up styles and added grid so it's the container the determines then size of filecards

--- stories/components/FileCard.js
+++ stories/components/FileCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { storiesOf, action } from '@storybook/react';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 import { FileCard, Icon } from '../../lib';
 import StoryItem from '../styleguide/StoryItem';
 
@@ -15,70 +17,87 @@ storiesOf('Components', module).add('FileCard', () => (
       title="File Card"
       description="A file card showing a preview of the file."
     >
-      <FileCard
-        filename="cute_sheep_in_iceland.jpg"
-        label="Sheep in Iceland"
-        previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
-      >
-        {actions}
-      </FileCard>
+      <Row>
+        <Col xs={4}>
+          <FileCard
+            filename="cute_sheep_in_iceland.jpg"
+            label="Sheep in Iceland"
+            previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+          >
+            {actions}
+          </FileCard>
+        </Col>
+      </Row>
     </StoryItem>
 
     <StoryItem
       title="File Card (highlighted)"
       description="A file card can have a highlighted state."
     >
-      <FileCard
-        filename="sheep_in_iceland.jpg"
-        label="Sheep in Iceland"
-        previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
-        isHighlighted
-      >
-        {actions}
-      </FileCard>
+      <Row>
+        <Col xs={4}>
+          <FileCard
+            filename="sheep_in_iceland.jpg"
+            label="Sheep in Iceland"
+            previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+            isHighlighted
+          >
+            {actions}
+          </FileCard>
+        </Col>
+      </Row>
     </StoryItem>
 
     <StoryItem
       title="File Card (without preview)"
       description="A file card without a preview image"
     >
-      <FileCard
-        filename="field_notes.txt"
-        label="Field notes"
-      >
-        {actions}
-      </FileCard>
+      <Row>
+        <Col xs={4}>
+          <FileCard filename="field_notes.txt" label="Field notes">
+            {actions}
+          </FileCard>
+        </Col>
+      </Row>
     </StoryItem>
 
     <StoryItem
       title="Multiple File Cards"
       description="Multiple file cards side by side"
     >
-      <FileCard
-        filename="sheep_in_iceland.jpg"
-        label="Sheep in Iceland"
-        previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
-        isHighlighted
-      >
-        {actions}
-      </FileCard>
-      <FileCard
-        type="image"
-        filename="sunset_in_berlin_large_filename.jpg"
-        label="Large sunset in Berlin, Germany"
-        previewSrc="http://www.sdpsnet.org/sdps/images/conference/2012/hotel/mod_galleries_53.jpeg"
-        isHighlighted
-      >
-        {actions}
-      </FileCard>
-      <FileCard
-        filename="sheep_in_iceland.jpg"
-        label="Sheep in Iceland"
-        previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
-        isHighlighted
-      >
-        {actions}
-      </FileCard>
+      <Row>
+        <Col xs={12} sm={4}>
+          <FileCard
+            filename="sheep_in_iceland.jpg"
+            label="Sheep in Iceland"
+            previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+            isHighlighted
+          >
+            {actions}
+          </FileCard>
+        </Col>
+        <Col xs={12} sm={4}>
+          <FileCard
+            type="image"
+            filename="sunset_in_berlin_large_filename.jpg"
+            label="Large sunset in Berlin, Germany"
+            previewSrc="http://www.sdpsnet.org/sdps/images/conference/2012/hotel/mod_galleries_53.jpeg"
+            isHighlighted
+          >
+            {actions}
+          </FileCard>
+        </Col>
+        <Col xs={12} sm={4}>
+          <FileCard
+            filename="sheep_in_iceland.jpg"
+            label="Sheep in Iceland"
+            previewSrc="https://icelanddefrosted.files.wordpress.com/2013/09/20130926-144345.jpg?w=922"
+            isHighlighted
+          >
+            {actions}
+          </FileCard>
+        </Col>
+      </Row>
     </StoryItem>
   </div>
 ));
