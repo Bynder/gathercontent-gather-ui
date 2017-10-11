706b8e126b2764a7a6dc8792d42d47171b0bdfe6
Mat Chapman <mat@gathercontent.com>
2017-10-11 18:07:05 +0100

    Prevent image flicker refresh

--- tests/FileCard/index.js
+++ tests/FileCard/index.js
@@ -1,8 +1,10 @@
-import { React, expect, shallow } from '../setup';
+import { React, expect, sinon, shallow } from '../setup';
 import FileCard from '../../lib/FileCard';
 
 describe('FileCard', () => {
   let wrapper;
+  let randomSpy;
+  let sinonClock;
 
   const props = {
     filename: 'field_notes.txt',
@@ -12,6 +14,8 @@ describe('FileCard', () => {
   };
 
   beforeEach(() => {
+    sinonClock = sinon.useFakeTimers();
+    randomSpy = sinon.stub(Math, `random`, () => 0.5);
     wrapper = shallow(
       <FileCard {...props}>
         <button>Test action</button>
@@ -19,17 +23,83 @@ describe('FileCard', () => {
     );
   });
 
+  afterEach(() => {
+    sinonClock.restore();
+    randomSpy.restore();
+  });
+
+  it('does not update the state when the image fails to load 5 times', () => {
+    wrapper.find('img').prop('onError')();
+    sinonClock.tick(5000);
+
+    expect(wrapper.state()).to.deep.equal({
+      imageCheckCounter: 1,
+      backgroundImage: `${props.previewSrc}?rand=0.05`,
+      disabled: false,
+      imageHasLoaded: false
+    });
+
+    wrapper.find('img').prop('onError')();
+    sinonClock.tick(5000);
+    expect(wrapper.state().imageCheckCounter).to.equal(2);
+
+    wrapper.find('img').prop('onError')();
+    sinonClock.tick(5000);
+    wrapper.find('img').prop('onError')();
+    sinonClock.tick(5000);
+    wrapper.find('img').prop('onError')();
+    sinonClock.tick(5000);
+    expect(wrapper.state().imageCheckCounter).to.equal(5);
+    wrapper.find('img').prop('onError')();
+    sinonClock.tick(5000);
+
+    expect(wrapper.state()).to.deep.equal({
+      imageCheckCounter: 5,
+      backgroundImage: `${props.previewSrc}?rand=0.05`,
+      disabled: false,
+      imageHasLoaded: false
+    });
+  });
+
+  it('updates the state when image fails to load', () => {
+    wrapper.find('img').prop('onError')();
+    sinonClock.tick(5000);
+    expect(wrapper.state()).to.deep.equal({
+      imageCheckCounter: 1,
+      backgroundImage: `${props.previewSrc}?rand=0.05`,
+      disabled: false,
+      imageHasLoaded: false
+    });
+  });
+
+  it('updates the state when the image has loaded', () => {
+    expect(wrapper.state().imageHasLoaded).to.equal(false);
+    wrapper.find('img').prop('onLoad')();
+    expect(wrapper.state().imageHasLoaded).to.equal(true);
+  });
+
+  it('does not update the failed imageCheckCounter when the image has loaded', () => {
+    expect(wrapper.state()).to.deep.equal({
+      imageCheckCounter: 0,
+      backgroundImage: props.previewSrc,
+      disabled: false,
+      imageHasLoaded: false
+    });
+
+    wrapper.find('img').prop('onError')();
+    sinonClock.tick(5000);
+    expect(wrapper.state().imageCheckCounter).to.equal(1);
+
+    wrapper.find('img').prop('onLoad')();
+    expect(wrapper.state().imageHasLoaded).to.equal(true);
+  });
+
   it('renders a file card component', () => {
     expect(wrapper.find('.file-card__wrapper')).to.have.length(1);
   });
 
-  it('renders an element with the correct background-image', () => {
-    expect(
-      wrapper
-        .find('img')
-        .props()
-        .src.indexOf(props.previewSrc)
-    ).to.equal(0);
+  it('renders an element with the correct image src', () => {
+    expect(wrapper.find('img').props().src).to.equal(props.previewSrc);
   });
 
   it('renders the correct filename and label for a thumbnail', () => {
