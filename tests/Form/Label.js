a3b6c3d996fd0c4c2bd2b8b130a9104a58f2d821
Kyle Shrives <kyle@gathercontent.com>
2017-11-07 09:39:12 +0000

    Added highlighted state to form inputs

--- tests/Form/Label.js
+++ tests/Form/Label.js
@@ -2,19 +2,35 @@ import { React, expect, shallow } from '../setup';
 import Label from '../../lib/Form/Label';
 
 describe('Label', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Label id="123" label="foo bar" />);
+  });
+
   it('has a html label with a "for" attribute', () => {
-    const wrapper = shallow(<Label id="123" label="foo bar" />);
     expect(wrapper.find('label').prop('htmlFor')).to.equal('123');
-    expect(wrapper.find('label').prop('children')).to.include('foo bar');
+    expect(
+      wrapper
+        .find('label')
+        .find('.form-input__text')
+        .prop('children')
+    ).to.include('foo bar');
   });
 
   it('may have a subtitle', () => {
-    const wrapper = shallow(
-      <Label id="123" label="foo bar" subtitle="some description" />
-    );
+    wrapper.setProps({ subtitle: 'some description' });
     const expectedSubtitle = (
       <span className="form-checkbox__subtitle">some description</span>
     );
     expect(wrapper.find('label').prop('children')).to.include(expectedSubtitle);
+  });
+
+  it('adds a highlight modifier to the text', () => {
+    wrapper.setProps({ highlight: true });
+    const hasHighlightClass = wrapper
+      .find('.form-input__text')
+      .hasClass('form__input-text--highlight');
+    expect(hasHighlightClass).to.equal(true);
   });
 });
