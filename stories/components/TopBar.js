88d0d84413286bf590ca78e0cd17603191d04c4e
Mat Chapman <mat@gathercontent.com>
2017-11-13 12:41:58 +0000

    Make a more substantial story

--- stories/components/TopBar.js
+++ stories/components/TopBar.js
@@ -4,6 +4,9 @@ import { storiesOf } from '@storybook/react';
 import StoryItem from '../styleguide/StoryItem';
 import TopBar from '../../lib/TopBar';
 import Button from '../../lib/Button';
+import Icon from '../../lib/Icon';
+import Avatar from '../../lib/Avatar';
+import AvatarGroup from '../../lib/AvatarGroup';
 
 storiesOf('Components', module).add('TopBar', () => (
   <div>
@@ -14,10 +17,49 @@ storiesOf('Components', module).add('TopBar', () => (
       <TopBar>
         <Col className="top-bar__content top-bar__content--left" xs={10} md={5}>
           <div className="top-bar__cell">
-            <Button types={['primary']} clickHandler={() => {}}>
-              Primary button
+            <Icon name="backArrow" className="top-bar__action top-bar__action--collapse-left" />
+          </div>
+          <div className="top-bar__cell top-bar__cell--bordered">
+            <Button
+              types={['icon-only']}
+              clickHandler={() => {}}
+              className="top-bar__action"
+            >
+              <Icon name="menu" size="minor" />
             </Button>
           </div>
+        </Col>
+        <Col className="top-bar__content top-bar__content--right" xs={2} md={4}>
+          <div className="top-bar__cell">
+            <AvatarGroup maximum={2}>
+              <Avatar
+                initials="RS"
+                name="Richard Swagshaw"
+                colour="rgb(252, 92, 84)"
+              />
+              <Avatar
+                initials="JD"
+                name="James Darracott"
+                colour="rgb(61, 138, 235)"
+              />
+            </AvatarGroup>
+          </div>
+          <div className="top-bar__cell top-bar__cell--bordered">
+            <Button
+              types={['icon-only']}
+              className="top-bar__action"
+              clickHandler={() => {}}
+            >
+              <Icon name="menuDotted" />
+            </Button>
+          </div>
+        </Col>
+        <Col
+          className="top-bar__content top-bar__content--center"
+          xs={12}
+          md={3}
+        >
+          Centered content
         </Col>
       </TopBar>
     </StoryItem>
