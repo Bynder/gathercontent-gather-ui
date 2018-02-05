1150154508927d1075982d587a67ffaf248c4196
Mat Chapman <mat@gathercontent.com>
2018-02-05 10:19:41 +0000

    Add action to story

--- stories/components/StatusIndicator.js
+++ stories/components/StatusIndicator.js
@@ -1,50 +1,51 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import StatusIndicator from '../../lib/StatusIndicator';
+import Button from '../../lib/Button';
 import StoryItem from '../styleguide/StoryItem';
 
-storiesOf('Components', module)
-  .add('StatusIndicator', () => {
-    return (
-      <div>
-        <StoryItem title="Status Indicator" description="Current">
-          <StatusIndicator
-            color="rgb(250, 167, 50)"
-            label="Editing"
-            current
-            description={'Edits to be made in response to user testing and the feedback from the senior editor and subject matter expert.'}
-          >
-            due Tommorrow at 12:00pm
-          </StatusIndicator>
-        </StoryItem>
+storiesOf('Components', module).add('StatusIndicator', () => (
+  <div>
+    <StoryItem title="Status Indicator" description="Current">
+      <StatusIndicator
+        color="rgb(250, 167, 50)"
+        label="Editing"
+        current
+        description={
+          'Edits to be made in response to user testing and the feedback from the senior editor and subject matter expert.'
+        }
+        actions={<Button types={['primary']} clickHandler={()=>{}}>Test Button</Button>}
+      >
+        Due Tommorrow at 12:00pm
+      </StatusIndicator>
+    </StoryItem>
 
-        <StoryItem description="Completed">
-          <StatusIndicator color="#3d8aeb" label="Review" completed>
-            due Tommorrow at 12:00pm
-          </StatusIndicator>
-        </StoryItem>
+    <StoryItem description="Completed">
+      <StatusIndicator color="#3d8aeb" label="Review" completed>
+        Due Tommorrow at 12:00pm
+      </StatusIndicator>
+    </StoryItem>
 
-        <StoryItem description="Bordered">
-          <StatusIndicator color="#3d8aeb" label="Review" bordered>
-            due Tommorrow at 12:00pm
-          </StatusIndicator>
-        </StoryItem>
+    <StoryItem description="Bordered">
+      <StatusIndicator color="#3d8aeb" label="Review" bordered>
+        Due Tommorrow at 12:00pm
+      </StatusIndicator>
+    </StoryItem>
 
-        <StoryItem description="Consists of a color and a label to express a status.">
-          <StatusIndicator color="#ff6300" label="Draft">
-            due Tommorrow at 12:00pm
-          </StatusIndicator>
-        </StoryItem>
+    <StoryItem description="Consists of a color and a label to express a status.">
+      <StatusIndicator color="#ff6300" label="Draft">
+        Due Tommorrow at 12:00pm
+      </StatusIndicator>
+    </StoryItem>
 
-        <StoryItem description="">
-          <StatusIndicator color="#93724f" label="Publishing">
-            set a due date
-          </StatusIndicator>
-        </StoryItem>
+    <StoryItem description="">
+      <StatusIndicator color="#93724f" label="Publishing">
+        set a due date
+      </StatusIndicator>
+    </StoryItem>
 
-        <StoryItem description="Optional children">
-          <StatusIndicator color="#965de8" label="Approval" />
-        </StoryItem>
-      </div>
-    );
-  });
+    <StoryItem description="Optional children">
+      <StatusIndicator color="#965de8" label="Approval" />
+    </StoryItem>
+  </div>
+));
