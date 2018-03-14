4d8103d49b877aeb050be44abc69601b86cf26b9
Mat Chapman <mat@gathercontent.com>
2018-03-14 21:52:15 +0000

    Add stories

--- stories/components/DueDatePicker.js
+++ stories/components/DueDatePicker.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf, action } from '@storybook/react';
 import DueDatePicker from '../../lib/DueDatePicker';
+import DueDateLabel from '../../lib/DueDatePicker/DueDateLabel';
 import StoryItem from '../styleguide/StoryItem';
 import moment from 'moment';
 
@@ -27,13 +28,6 @@ storiesOf('Components', module).add('DueDatePicker', () => (
         setDueDate={action('setDueDate')}
       />
     </StoryItem>
-    <StoryItem title="Due date picker" description="+4 Days">
-      <DueDatePicker
-        label="Due Tommorrow at 12:00pm"
-        dueDate={moment().add(4, 'day')}
-        setDueDate={action('setDueDate')}
-      />
-    </StoryItem>
     <StoryItem title="Due date picker" description="-4 Days">
       <DueDatePicker
         label="Due Tommorrow at 12:00pm"
@@ -46,6 +40,30 @@ storiesOf('Components', module).add('DueDatePicker', () => (
         label="Due Tommorrow at 12:00pm"
         dueDate={moment().subtract(4, 'months')}
         setDueDate={action('setDueDate')}
+      />
+    </StoryItem>
+    <StoryItem title="Due date picker" description="-4 Months (Completed)">
+      <DueDatePicker
+        label="Due Tommorrow at 12:00pm"
+        dueDate={moment().subtract(4, 'months')}
+        setDueDate={action('setDueDate')}
+        completed
+      />
+    </StoryItem>
+  </div>
+)).add('DueDateLabel', () => (
+  <div>
+    <StoryItem title="Due date picker" description="No date">
+      <DueDateLabel />
+    </StoryItem>
+    <StoryItem title="Due date picker" description="+1 Day">
+      <DueDateLabel
+        dueDate={{date:moment().add(1, 'day').set({hours:14,minutes:15})}}
+      />
+    </StoryItem>
+    <StoryItem title="Due date picker" description="-2 Days">
+      <DueDateLabel
+        dueDate={{date:moment().subtract(2, 'day'), overdue: true}}
       />
     </StoryItem>
   </div>
