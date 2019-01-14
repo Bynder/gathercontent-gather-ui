1b865c583a610102e1f0c38a904c03b17477c231
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2019-01-14 16:04:27 +0000

    Add FormModal example to Modal stories

--- stories/components/Modals.js
+++ stories/components/Modals.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import { Modal, Button, ConfirmationModal, withModalTrigger } from '../../lib';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import {
+  Modal,
+  Button,
+  ConfirmationModal,
+  withModalTrigger,
+  FormModal
+} from '../../lib';
 import StoryItem from '../styleguide/StoryItem';
 
 const ModalTrigger = withModalTrigger({ children: 'Show Modal' });
+
+class StatefulFormModal extends Component {
+  state = {
+    formIsSubmitting: false
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+    this.setState({ formIsSubmitting: true });
+    action('submit')(e);
+
+    // asynchronous event, eg. HTTP request
+    setTimeout(() => {
+      this.setState({ formIsSubmitting: false });
+      action('submitHandlerReturned')();
+    }, 2000);
+  };
+
+  render() {
+    return (
+      <FormModal
+        {...this.props}
+        formIsSubmitting={this.state.formIsSubmitting}
+        submitHandler={this.submitHandler}
+      />
+    );
+  }
+}
 
 storiesOf('Components', module).add('Modals', () => (
   <div>
@@ -182,93 +219,113 @@ storiesOf('Components', module).add('Modals', () => (
       </ModalTrigger>
     </StoryItem>
 
-    <StoryItem>
-      <StoryItem
-        title="Overflow Modal"
-        description="A modal which has content that overflows the screen."
-      >
-        <ModalTrigger>
-          <Modal.Container overflow>
-            <Modal.Header>Hello</Modal.Header>
-            <Modal.Body className="has-columns">
-              <Row>
-                <Col
-                  className="modal__column-wrapper modal__column-wrapper--highlight"
-                  xs={6}
-                >
-                  <div className="modal__column">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi
-                    ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit, sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident Lorem ipsum
-                    dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                    ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                    dolor in reprehenderit in voluptate velit esse cillum dolore
-                    eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                    non proident Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident Lorem ipsum
-                    dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                    ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                    dolor in reprehenderit in voluptate velit esse cillum dolore
-                    eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                    non proident Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident
-                  </div>
-                </Col>
-                <Col className="modal__column-wrapper" xs={6}>
-                  <div className="modal__column">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident
-                  </div>
-                </Col>
-              </Row>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button types={['link']} clickHandler={action('clickHandler')}>
-                Cancel
-              </Button>
-              <Button types={['primary']} clickHandler={action('clickHandler')}>
-                Confirm
-              </Button>
-            </Modal.Footer>
-          </Modal.Container>
-        </ModalTrigger>
-      </StoryItem>
+    <StoryItem
+      title="Overflow Modal"
+      description="A modal which has content that overflows the screen."
+    >
+      <ModalTrigger>
+        <Modal.Container overflow>
+          <Modal.Header>Hello</Modal.Header>
+          <Modal.Body className="has-columns">
+            <Row>
+              <Col
+                className="modal__column-wrapper modal__column-wrapper--highlight"
+                xs={6}
+              >
+                <div className="modal__column">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident Lorem ipsum dolor sit amet, consectetur
+                  adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                  sint occaecat cupidatat non proident Lorem ipsum dolor sit
+                  amet, consectetur adipisicing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident Lorem ipsum dolor sit amet, consectetur
+                  adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                  sint occaecat cupidatat non proident Lorem ipsum dolor sit
+                  amet, consectetur adipisicing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident
+                </div>
+              </Col>
+              <Col className="modal__column-wrapper" xs={6}>
+                <div className="modal__column">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident
+                </div>
+              </Col>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button types={['link']} clickHandler={action('clickHandler')}>
+              Cancel
+            </Button>
+            <Button types={['primary']} clickHandler={action('clickHandler')}>
+              Confirm
+            </Button>
+          </Modal.Footer>
+        </Modal.Container>
+      </ModalTrigger>
+    </StoryItem>
+
+    <StoryItem
+      title="FormModal"
+      description="A modal which can contain a form. It has a progress button to indicate progress while submitting"
+    >
+      <ModalTrigger>
+        <StatefulFormModal
+          title="FormModal example"
+          submitText="Submit"
+          cancelText="Cancel"
+          cancelHandler={action('cancel')}
+        >
+          <FormGroup>
+            <ControlLabel>Field A</ControlLabel>
+            <FormControl />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Field B</ControlLabel>
+            <FormControl />
+          </FormGroup>
+        </StatefulFormModal>
+      </ModalTrigger>
     </StoryItem>
   </div>
 ));
