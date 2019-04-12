c5b759fe076544e229d95eb2013b75209a702324
harperllama <kyle@kyleshrives.co.uk>
2019-04-12 14:57:14 +0100

    added new pricing components and removed old PlanBox ones

--- stories/components/Pricing.js
+++ stories/components/Pricing.js
@@ -1,136 +1,105 @@
-import React from 'react';
+import React, { Fragment } from "react";
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import PlanBox from '../../lib/PlanBox';
-import PlanBoxPricing from '../../lib/PlanBox/Pricing';
-import PlanBoxWrapper from '../../lib/PlanBox/Wrapper';
-import PlanBoxAllowanceDetails from '../../lib/PlanBox/AllowanceDetails';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 import StoryItem from '../styleguide/StoryItem';
+import { PricingWrapper, PricingPlan, FeatureList, FeatureListItem, Icon, TooltipWrapper } from "../../lib";
+import PersonSVG from '../../assets/person.svg';
+
+const features = [{
+  name: 'Real-time content editor',
+  standardEnabled: true,
+  advancedEnabled: false,
+}, {
+  name: 'Structured templates',
+  standardEnabled: true,
+  advancedEnabled: true,
+}, {
+  name: 'Unlimited items',
+  standardEnabled: true,
+  advancedEnabled: true,
+}, {
+  name: 'Unlimited projects',
+  standardEnabled: true,
+  advancedEnabled: false,
+}, {
+  name: 'Advanced search',
+  standardEnabled: true,
+  advancedEnabled: false,
+}];
+
+const upgradeButton = (
+  <a className="button button--primary" href="/">Upgrade</a>
+);
+
+const featureListHeadings = (
+  <FeatureList title="Features" row>
+    {features.map(({ name }) => (
+      <FeatureListItem key={name}>
+        <TooltipWrapper id={name} tooltipText="Tooltip text here..." placement="top">
+          {name}
+        </TooltipWrapper>
+      </FeatureListItem>
+    ))}
+  </FeatureList>
+);
+
+const featureListStandard = (
+  <FeatureList title="Features">
+    {features.map(({ name, standardEnabled }) => (
+      <FeatureListItem key={name}>
+        {name}
+        {(standardEnabled) ? <Icon name="tickCircle" className="fill-primary-green" /> : <Icon name="emptyCircle" />}
+      </FeatureListItem>
+    ))}
+  </FeatureList>
+);
+
+const featureListAdvanced = (
+  <FeatureList title="Features">
+    {features.map(({ name, advancedEnabled }) => (
+      <FeatureListItem key={name}>
+        {name}
+        {(advancedEnabled) ? <Icon name="tickCircle" className="fill-primary-green" /> : <Icon name="emptyCircle" />}
+      </FeatureListItem>
+    ))}
+  </FeatureList>
+);
 
 storiesOf('Components', module)
   .add('Pricing Plans', () => {
     return (
       <div>
         <StoryItem
-          title="Pricing Plans"
-          description="The plan boxes to display an entire plan type in the pricing section. It's made up of three different composable components: PlanBox, PlanBoxPricing and PlanBoxAllowanceDetails"
+          title="Pricing"
+          description="..."
         >
-          <PlanBoxWrapper>
-            <PlanBox
-              disabled={true}
-              name="Plan name"
-              description="Plan description"
-              upgradeUrl="#upgrade"
-              buttonText="Button text"
-              tooltipText="Exceeded item limit"
-            >
-              <PlanBoxPricing
-                price={100}
-                priceIn="/mo"
-                priceType="priceMonthly"
-              />
-              <PlanBoxAllowanceDetails
-                plan={{ items: 100, projects: 10 }}
-              />
-            </PlanBox>
-            <PlanBox
-              disabled={false}
-              name="Plan name"
-              description="Plan description"
-              upgradeUrl="#"
-              buttonText="Button text"
-              tooltipText="Exceeded item limit"
-            >
-              <PlanBoxPricing
-                price={100}
-                priceIn="/mo"
-                priceType="priceMonthly"
-              />
-              <PlanBoxAllowanceDetails
-                plan={{ items: 100, projects: 10 }}
-              />
-            </PlanBox>
-            <PlanBox
-              disabled={false}
-              name="Plan name"
-              description="Plan description"
-              upgradeUrl="#upgrade"
-              buttonText="Button text"
-              tooltipText="Exceeded item limit"
-            >
-              <PlanBoxPricing
-                price={100}
-                priceIn="/mo"
-                priceType="priceMonthly"
-              />
-              <PlanBoxAllowanceDetails
-                plan={{ items: 100, projects: 10 }}
-              />
-            </PlanBox>
-          </PlanBoxWrapper>
-        </StoryItem>
+          <PricingWrapper>
+            {featureListHeadings}
+            {featureListHeadings}
+            {featureListHeadings}
 
-        <StoryItem title="Pricing Plans (Collapsed Layout)">
-          <PlanBoxWrapper
-            className="plan-box__wrapper--recommend-collapse"
-            gridClassName="grid--collapse"
-            gridCellClassName="grid__cell--collapse"
-          >
-            <PlanBox
-              disabled={true}
-              name="Plan name"
-              description="Plan description"
-              upgradeUrl="#upgrade"
-              buttonText="Button text"
-              tooltipText="Exceeded item limit"
-            >
-              <PlanBoxPricing
-                price={100}
-                priceIn="/mo"
-                priceType="priceMonthly"
-              />
-              <PlanBoxAllowanceDetails
-                plan={{ items: 100, projects: 10 }}
-              />
-            </PlanBox>
-            <PlanBox
-              recommended={true}
-              disabled={false}
-              name="Plan name"
-              description="Plan description"
-              upgradeUrl="#"
-              buttonText="Button text"
-              tooltipText="Exceeded item limit"
-            >
-              <PlanBoxPricing
-                price={100}
-                originalPrice={150}
-                savings={50}
-                priceIn="/mo"
-                priceType="priceMonthly"
-              />
-              <PlanBoxAllowanceDetails
-                plan={{ items: 100, projects: 10 }}
-              />
-            </PlanBox>
-            <PlanBox
-              disabled={false}
-              name="Plan name"
-              description="Plan description"
-              upgradeUrl="#upgrade"
-              buttonText="Button text"
-              tooltipText="Exceeded item limit"
-            >
-              <PlanBoxPricing
-                price={100}
-                priceIn="/mo"
-                priceType="priceMonthly"
-              />
-              <PlanBoxAllowanceDetails
-                plan={{ items: 100, projects: 10 }}
-              />
-            </PlanBox>
-          </PlanBoxWrapper>
+            <Row>
+              <Col xs={12} md={6}>
+                <PricingPlan price="$15" smallPrint="$750 per month" priceDesc="per seat per month" title="Standard" upgradeButton={upgradeButton} savings="Save $360">
+                  {featureListStandard}
+                  {featureListStandard}
+                  {featureListStandard}
+                </PricingPlan>
+              </Col>
+
+              <Col xs={12} md={6}>
+                <PricingPlan price="$25" smallPrint="$1,250 per month" priceDesc="per seat per month" title="Advanced" upgradeButton={upgradeButton} savings="Save $360">
+                  <div className="pricing__plan-person">
+                    <PersonSVG />
+                  </div>
+                  {featureListAdvanced}
+                  {featureListAdvanced}
+                  {featureListAdvanced}
+                </PricingPlan>
+              </Col>
+            </Row>
+          </PricingWrapper>
         </StoryItem>
       </div>
     );
