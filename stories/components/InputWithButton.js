f8a47a1be7f61b57217fd9f9ab098a88160d6f53
Alex Smith <altaudiolabs@gmail.com>
2019-05-23 10:53:04 +0100

    Styles the input

--- stories/components/InputWithButton.js
+++ stories/components/InputWithButton.js
@@ -7,14 +7,14 @@ storiesOf('Components', module).add('InputWithButton', () => (
   <div>
     <StoryItem title="InputWithButton" description="A component to display some text and a button">
       <div>
-        <InputWithButton value="text" onClick={() => alert('Clicked')} buttonText="Click Me" />
+        <InputWithButton value="text" onClick={value => alert(value)} buttonText="Click Me" />
       </div>
     </StoryItem>
     <StoryItem title="InputWithButton Long Text" description="InputWithButton long text value">
       <div>
         <InputWithButton 
           value="https://www.google.com/search?safe=off&source=hp&ei=-mfmXMLbCdChkwWAvpnIDg&q=hello&oq=hello" 
-          onClick={() => alert('Clicked')} 
+          onClick={value => alert(value)} 
           buttonText="Click Me"             
         />
       </div>
