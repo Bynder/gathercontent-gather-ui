8bcc71c43dc7e504d1ef8774420f1084089aa542
harperllama <kyle@kyleshrives.co.uk>
2019-06-27 08:53:25 +0100

    fleshed out breadcrumbs

--- stories/components/Breadcrumb.js
+++ stories/components/Breadcrumb.js
@@ -1,31 +1,90 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Breadcrumb, Icon } from '../../lib';
+import { Dropdown, Breadcrumb, Icon } from '../../lib';
 import StoryItem from '../styleguide/StoryItem';
+
+const dropdown = (
+  <Dropdown id="1">
+    <Dropdown.Trigger>
+      <Icon name="menuDotted" />
+    </Dropdown.Trigger>
+
+    <Dropdown.Content collapse>
+      <Dropdown.ActionGroup>
+        <Dropdown.Action action={() => {}}>
+          Rename folder
+        </Dropdown.Action>
+        <Dropdown.Action action={() => {}}>
+          Copy URL
+        </Dropdown.Action>
+      </Dropdown.ActionGroup>
+      <Dropdown.ActionGroup>
+        <Dropdown.Action action={() => {}}>
+          Move to trash
+        </Dropdown.Action>
+      </Dropdown.ActionGroup>
+    </Dropdown.Content>
+  </Dropdown>
+);
 
 storiesOf('Components', module).add('Breadcrumb', () => (
   <div>
-    <StoryItem
-      title="Avatar – base"
-      description="A base avatar will fall back to initials if no url prop is passed."
-    >
+    <StoryItem title="Breadcrumb">
       <Breadcrumb>
         <Breadcrumb.Item>
-          <a href="/">
-            <Icon name="home" />
-          </a>
+          <div className="text-overflow-ellipsis" title="Energy Co. Redesign">
+            Energy Co. Redesign
+          </div>
+          {dropdown}
+        </Breadcrumb.Item>
+      </Breadcrumb>
+    </StoryItem>
+
+    <StoryItem title="Breadcrumb">
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <div className="text-overflow-ellipsis" style={{ width: '100px' }} title="Energy Co. Redesign">
+            <a href="/">Energy Co. Redesign</a>
+          </div>
         </Breadcrumb.Item>
         <Breadcrumb.Item>
-          <Icon name="chevronRight" />
+          <div className="text-overflow-ellipsis" title="About">
+            <a href="/">About</a>
+          </div>
         </Breadcrumb.Item>
         <Breadcrumb.Item>
-          <a href="/">Link 2</a>
+          <div className="text-overflow-ellipsis" title="History">
+            History
+          </div>
+          {dropdown}
+        </Breadcrumb.Item>
+      </Breadcrumb>
+    </StoryItem>
+
+    <StoryItem title="Breadcrumb">
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <Dropdown id="1">
+            <Dropdown.Trigger>
+              ...
+            </Dropdown.Trigger>
+            <Dropdown.Content collapse>
+              <Dropdown.ActionGroup>
+                <a href="/" className="dropdown__action" title="Energy Co. Redesign">Energy Co. Redesign</a>
+              </Dropdown.ActionGroup>
+            </Dropdown.Content>
+          </Dropdown>
         </Breadcrumb.Item>
         <Breadcrumb.Item>
-          <Icon name="chevronRight" />
+          <div className="text-overflow-ellipsis">
+            <a href="/" title="About">About</a>
+          </div>
         </Breadcrumb.Item>
         <Breadcrumb.Item>
-          <a href="/">Link 3</a>
+          <div className="text-overflow-ellipsis" title="History">
+            History
+          </div>
+          {dropdown}
         </Breadcrumb.Item>
       </Breadcrumb>
     </StoryItem>
