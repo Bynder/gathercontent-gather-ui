baf32ed37da9531ee6f8e17f0c0b36dcaef5f487
harperllama <kyle@kyleshrives.co.uk>
2019-07-24 12:00:08 +0100

    added all the goodies

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
-import { configure } from '@storybook/react';
-import { addCSS } from './utils';
-
+import { configure, addDecorator } from '@storybook/react';
 import './IBM-Plex/font.css';
+import { withA11y } from '@storybook/addon-a11y';
+
 require('../styles/bootstrap/_bootstrap.scss');
 require('../styles/styleguide/main.scss');
 require('../styles/main.scss');
@@ -10,5 +10,7 @@ require('font-awesome/css/font-awesome.css');
 function loadStories() {
   require('../stories/index.js');
 }
+
+addDecorator(withA11y);
 
 configure(loadStories, module);
--- stories/index.js
+++ stories/index.js
@@ -58,3 +58,4 @@ import UserSearch from './components/UserSearch';
 import ImageLoader from './components/images/ImageLoader';
 import InputWithButton from './components/InputWithButton';
 import Pill from './components/Pill';
+import HierarchyCollection from '../lib/HierarchyCollection/HierarchyCollectionStory';
