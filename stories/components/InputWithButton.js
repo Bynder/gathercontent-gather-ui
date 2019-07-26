b321a59dfb30631b79b53dc8683115285a680b52
Alex Smith <altaudiolabs@gmail.com>
2019-07-26 11:53:27 +0100

    stash

--- stories/components/InputWithButton.js
+++ stories/components/InputWithButton.js
@@ -1,44 +1,60 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
 import InputWithButton from '../../lib/InputWithButton';
 import StoryItem from '../styleguide/StoryItem';
+import mainTheme from '../../styles/themes/main';
+
 
 storiesOf('Components', module).add('InputWithButton', () => (
-  <div>
-    <StoryItem title="InputWithButton" description="A component to display some text and a button">
-      <div>
-        <InputWithButton value="text" onClick={value => alert(value)} buttonText="Click Me" />
-      </div>
-    </StoryItem>
-    <StoryItem title="InputWithButton Long Text" description="InputWithButton long text value">
-      <div>
-        <InputWithButton
-          value="https://www.google.com/search?safe=off&source=hp&ei=-mfmXMLbCdChkwWAvpnIDg&q=hello&oq=hello"
-          onClick={value => alert(value)}
-          buttonText="Click Me"
-        />
-      </div>
-    </StoryItem>
-    <StoryItem title="InputWithButton with button text that changes on click" description="Button text changes to buttonTextAfterClick prop onClick">
-      <div>
-        <InputWithButton
-          value="https://www.google.com/search?safe=off&source=hp&ei=-mfmXMLbCdChkwWAvpnIDg&q=hello&oq=hello"
-          onClick={value => {}}
-          buttonText="Click Me"
-          buttonTextAfterClick="I've been clicked!"
-        />
-      </div>
-    </StoryItem>
-    <StoryItem title="InputWithButton padding small">
-      <div>
-        <InputWithButton
-          value="https://www.google.com/search?safe=off&source=hp&ei=-mfmXMLbCdChkwWAvpnIDg&q=hello&oq=hello"
-          onClick={value => {}}
-          buttonText="Click Me"
-          buttonTextAfterClick="I've been clicked!"
-          paddingSmall
-        />
-      </div>
-    </StoryItem>
-  </div>
+  <ThemeProvider theme={mainTheme}>
+    <div>
+      <StoryItem title="InputWithButton" description="A component to display some text and a button">
+	<div>
+	  <InputWithButton value="text" onClick={value => alert(value)} buttonText="Click Me" />
+	</div>
+      </StoryItem>
+      <StoryItem title="InputWithButton Long Text" description="InputWithButton long text value">
+	<div>
+	  <InputWithButton
+	    value="https://www.google.com/search?safe=off&source=hp&ei=-mfmXMLbCdChkwWAvpnIDg&q=hello&oq=hello"
+	    onClick={value => alert(value)}
+	    buttonText="Click Me"
+	  />
+	</div>
+      </StoryItem>
+      <StoryItem title="InputWithButton with button text that changes on click" description="Button text changes to buttonTextAfterClick prop onClick">
+	<div>
+	  <InputWithButton
+	    value="https://www.google.com/search?safe=off&source=hp&ei=-mfmXMLbCdChkwWAvpnIDg&q=hello&oq=hello"
+	    onClick={value => {}}
+	    buttonText="Click Me"
+	    buttonTextAfterClick="I've been clicked!"
+	  />
+	</div>
+      </StoryItem>
+      <StoryItem title="InputWithButton padding small">
+	<div>
+	  <InputWithButton
+	    value="https://www.google.com/search?safe=off&source=hp&ei=-mfmXMLbCdChkwWAvpnIDg&q=hello&oq=hello"
+	    onClick={value => {}}
+	    buttonText="Click Me"
+	    buttonTextAfterClick="I've been clicked!"
+	    paddingSmall
+	  />
+	</div>
+      </StoryItem>
+      <StoryItem title="Disabled InputWithButton">
+	<div>
+	  <InputWithButton
+	    value="https://www.google.com/search?safe=off&source=hp&ei=-mfmXMLbCdChkwWAvpnIDg&q=hello&oq=hello"
+	    onClick={value => {}}
+	    buttonText="Click Me"
+	    buttonTextAfterClick="I've been clicked!"
+	    disabled
+	  />
+	</div>
+      </StoryItem>
+    </div>
+  </ThemeProvider>
 ));
---
+++ styles/themes/main.js
@@ -0,0 +1,29 @@
+export default {
+  colors: {
+    white: '#FF',
+    neutral: {
+      dark: 'rgb(41, 51, 61)',
+      base: 'rgb(133, 153, 173)',
+      light: 'rgb(224, 229, 234)',
+      lightest: 'rgb(249, 250, 251)',
+    }
+  },
+  fonts: {
+    sizes: {
+      xlarge: '1.5rem',
+      large: '1.25rem',
+      lead: '1.125rem',
+      base: '1rem',
+      slight: '.875rem',
+      small: '.75rem',
+      micro: '.6875rem',
+    },
+    lineHeights: {
+      base: '1.25rem',
+      lead: '1.5rem',
+    },
+  },
+  layout: {
+    spacingBase: 20
+  }
+}
