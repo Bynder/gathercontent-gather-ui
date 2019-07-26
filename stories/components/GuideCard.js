0f0e9e1d36d06631d817f73bf98e076e381e25e0
Alex Smith <altaudiolabs@gmail.com>
2019-07-26 16:18:07 +0100

    Starts the GuideCard.Body component

--- stories/components/GuideCard.js
+++ stories/components/GuideCard.js
@@ -8,10 +8,9 @@ storiesOf('Components', module).add('GuideCard', () => (
   <div>
     <StoryItem title="GuideCard">
       <GuideCard.Container icon="template">
-	  This is a card
+	  <GuideCard.Body stepNumber={1} title="Create the first template" />
       </GuideCard.Container>
       <GuideCard.Container icon="item">
-	  This is a card
       </GuideCard.Container>
     </StoryItem>
   </div>
