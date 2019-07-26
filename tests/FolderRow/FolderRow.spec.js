41a862aae3e7a8b8dd5b00972ac103054e9d3ba6
harperllama <kyle@kyleshrives.co.uk>
2019-07-26 11:32:45 +0100

    added tests

--- tests/FolderRow/FolderRow.spec.js
+++ tests/FolderRow/FolderRow.spec.js
@@ -1,26 +1,45 @@
 import { React, shallow } from '../setup';
-import { FolderRow } from '../../lib/FolderRow';
+import { FolderRow, Button } from '../../lib';
 
 describe('FolderRow', () => {
+  const props = {
+    name: 'folder name'
+  };
+
   test('rendering folder name', () => {
-    const wrapper = shallow(<FolderRow>folder name</FolderRow>);
+    const wrapper = shallow(<FolderRow {...props} />);
 
     expect(wrapper.text()).toEqual('folder name');
   });
 
   test('rendering meta', () => {
-    const wrapper = shallow(
-      <FolderRow metaText={<p>meta</p>}>folder name</FolderRow>
-    );
+    const wrapper = shallow(<FolderRow {...props} metaText={<p>meta</p>} />);
 
     expect(wrapper.find('p').text()).toEqual('meta');
   });
 
   test('rendering actions', () => {
-    const wrapper = shallow(
-      <FolderRow actions={<p>actions</p>}>folder name</FolderRow>
-    );
+    const wrapper = shallow(<FolderRow {...props} actions={<p>actions</p>} />);
 
     expect(wrapper.find('p').text()).toEqual('actions');
+  });
+
+  test('showing the toggle action', () => {
+    const wrapper = shallow(<FolderRow {...props} />);
+
+    expect(wrapper.find(Button)).toHaveLength(0);
+    wrapper.setProps({ showToggle: true });
+    expect(wrapper.find(Button)).toHaveLength(1);
+  });
+
+  test('rendering the folder contents', () => {
+    const wrapper = shallow(
+      <FolderRow {...props} showToggle>
+        <p>Hello world</p>
+      </FolderRow>
+    );
+    expect(wrapper.find('p')).toHaveLength(0);
+    wrapper.find(Button).simulate('click');
+    expect(wrapper.find('p')).toHaveLength(1);
   });
 });
