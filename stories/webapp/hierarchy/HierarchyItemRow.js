d6a95af47878b71908905949eadf79660de6edc5
harperllama <kyle@kyleshrives.co.uk>
2019-07-31 15:56:05 +0100

    added template name to view using new components and altering mocking helper so we can not render components if required.

--- stories/webapp/hierarchy/HierarchyItemRow.js
+++ stories/webapp/hierarchy/HierarchyItemRow.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import faker from 'faker';
 import { ItemRow, StatusIndicator } from '../../../lib';
-import { createAvatarGroup } from '../../../lib/Avatar/stories/createAvatarGroup';
+import { AvatarGroupMock } from '../../../lib/Avatar/stories/AvatarGroupMock';
 
 const createStatusIndicator = statusColor => (
-  <StatusIndicator color={statusColor} />
+  <StatusIndicator color={statusColor} className="h-margin-right-half" />
 );
 
 export const HierarchyItemRow = ({ statusColor }) => (
-  <ItemRow
-    className="h-margin-bottom-half"
-    indicator={createStatusIndicator(statusColor)}
-    participants={createAvatarGroup()}
-    bordered
-  >
-    <a href="/">{faker.commerce.productName()}</a>
+  <ItemRow className="h-margin-bottom-half" bordered>
+    <ItemRow.Name>
+      {createStatusIndicator(statusColor)}
+      <a href="/">{faker.commerce.productName()}</a>
+    </ItemRow.Name>
+
+    <ItemRow.Aside>
+      <ItemRow.Data>No template</ItemRow.Data>
+
+      <AvatarGroupMock defaultValue={3}>
+        {({ ui, count }) => (count ? <ItemRow.Data>{ui}</ItemRow.Data> : null)}
+      </AvatarGroupMock>
+    </ItemRow.Aside>
   </ItemRow>
 );
 
