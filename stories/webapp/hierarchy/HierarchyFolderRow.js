175af33cdd422433baed1d52351e8adb21a8026b
harperllama <kyle@kyleshrives.co.uk>
2019-08-12 11:58:07 +0100

    added renaming and fixed the width issue within item and folder rows so the input will span correctly

--- stories/webapp/hierarchy/HierarchyFolderRow.js
+++ stories/webapp/hierarchy/HierarchyFolderRow.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { number, oneOfType, node, arrayOf, bool } from 'prop-types';
 import faker from 'faker';
+import { action } from '@storybook/addon-actions';
 import { FolderRow } from '../../../lib';
+import EditableTextWrapper from '../../../lib/EditableTextWrapper';
 
-export const HierarchyFolderRow = ({ childCount, children, open }) => (
-  <FolderRow
-    className="h-margin-bottom-half"
-    metaText={`${childCount} items`}
-    name={faker.commerce.department()}
-    open={open}
-    showToggle
-    style={{ minWidth: '320px' }}
-  >
-    {children}
-  </FolderRow>
-);
+export const HierarchyFolderRow = ({ childCount, children, open }) => {
+  const name = faker.commerce.department();
+
+  return (
+    <FolderRow
+      className="h-margin-bottom-half"
+      metaText={`${childCount} items`}
+      name={
+        <EditableTextWrapper
+          value={name}
+          className="h-margin-clear h-width-100"
+          onChange={action('Folder name changed.')}
+        >
+          {name}
+        </EditableTextWrapper>
+      }
+      open={open}
+      showToggle
+      style={{ minWidth: '320px' }}
+    >
+      {children}
+    </FolderRow>
+  );
+};
 
 HierarchyFolderRow.propTypes = {
   childCount: number.isRequired,
--- stories/webapp/hierarchy/HierarchyItemRow.js
+++ stories/webapp/hierarchy/HierarchyItemRow.js
@@ -1,36 +1,49 @@
 import React from 'react';
 import faker from 'faker';
-import { ItemRow, StatusIndicator } from '../../../lib';
+import { action } from '@storybook/addon-actions';
+import { ItemRow, StatusIndicator, EditableTextWrapper } from '../../../lib';
 import { AvatarGroupMock } from '../../../lib/Avatar/stories/AvatarGroupMock';
 
 const createStatusIndicator = statusColor => (
   <StatusIndicator color={statusColor} className="h-margin-right-half" />
 );
 
-export const HierarchyItemRow = ({ statusColor }) => (
-  <ItemRow
-    className="h-margin-bottom-half"
-    bordered
-    style={{ minWidth: '320px' }}
-  >
-    <ItemRow.Name>
-      {createStatusIndicator(statusColor)}
-      <a href="/">{faker.commerce.productName()}</a>
-    </ItemRow.Name>
+export const HierarchyItemRow = ({ statusColor }) => {
+  const name = faker.commerce.productName();
 
-    <ItemRow.Aside>
-      <ItemRow.Data>No template</ItemRow.Data>
+  return (
+    <ItemRow
+      className="h-margin-bottom-half"
+      bordered
+      style={{ minWidth: '320px' }}
+    >
+      <ItemRow.Name>
+        {createStatusIndicator(statusColor)}
+        <EditableTextWrapper
+          value={name}
+          className="h-margin-clear h-width-100"
+          onChange={action('Item name changed.')}
+        >
+          <a href="/">{name}</a>
+        </EditableTextWrapper>
+      </ItemRow.Name>
 
-      <AvatarGroupMock
-        defaultMaxCount={8}
-        avatarProps={{ smallSize: true, bordered: true }}
-        avatarGroupProps={{ small: true }}
-      >
-        {({ ui, count }) => (count ? <ItemRow.Data>{ui}</ItemRow.Data> : null)}
-      </AvatarGroupMock>
-    </ItemRow.Aside>
-  </ItemRow>
-);
+      <ItemRow.Aside>
+        <ItemRow.Data>No template</ItemRow.Data>
+
+        <AvatarGroupMock
+          defaultMaxCount={8}
+          avatarProps={{ smallSize: true, bordered: true }}
+          avatarGroupProps={{ small: true }}
+        >
+          {({ ui, count }) =>
+            count ? <ItemRow.Data>{ui}</ItemRow.Data> : null
+          }
+        </AvatarGroupMock>
+      </ItemRow.Aside>
+    </ItemRow>
+  );
+};
 
 HierarchyItemRow.propTypes = {
   statusColor: StatusIndicator.propTypes.color.isRequired
