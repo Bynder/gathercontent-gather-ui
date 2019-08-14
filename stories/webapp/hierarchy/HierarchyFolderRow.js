4a167d9a27c8c618826ff22111193c9ce67f70e7
harperllama <kyle@kyleshrives.co.uk>
2019-08-14 13:43:49 +0100

    fixed some issues around default props

--- stories/webapp/hierarchy/HierarchyFolderRow.js
+++ stories/webapp/hierarchy/HierarchyFolderRow.js
@@ -1,41 +1,45 @@
 import React from 'react';
-import { number, oneOfType, node, arrayOf, bool } from 'prop-types';
 import faker from 'faker';
 import { action } from '@storybook/addon-actions';
-import { FolderRow } from '../../../lib';
+import { Button, FolderRow, Icon, TooltipWrapper } from '../../../lib';
 import EditableTextWrapper from '../../../lib/EditableTextWrapper';
 
-export const HierarchyFolderRow = ({ childCount, children, open }) => {
-  const name = faker.commerce.department();
+export const HierarchyFolderRow = ({ name, childCount, children, open }) => (
+  <FolderRow
+    className="h-margin-bottom-half"
+    metaText={`${childCount} items`}
+    name={
+      <EditableTextWrapper
+        value={name}
+        className="h-margin-clear"
+        onChange={action('Folder name changed.')}
+      >
+        {name}
+      </EditableTextWrapper>
+    }
+    actions={
+      <TooltipWrapper
+        id="new folder"
+        tooltipText="New folder"
+        className="folder-row__action"
+        placement="top"
+      >
+        <Button types={['icon-only']} onClick={() => {}}>
+          <Icon name="folderNew" />
+        </Button>
+      </TooltipWrapper>
+    }
+    open={open}
+    showToggle
+    style={{ minWidth: '320px' }}
+  >
+    <>{children}</>
+  </FolderRow>
+);
 
-  return (
-    <FolderRow
-      className="h-margin-bottom-half"
-      metaText={`${childCount} items`}
-      name={
-        <EditableTextWrapper
-          value={name}
-          className="h-margin-clear"
-          onChange={action('Folder name changed.')}
-        >
-          {name}
-        </EditableTextWrapper>
-      }
-      open={open}
-      showToggle
-      style={{ minWidth: '320px' }}
-    >
-      {children}
-    </FolderRow>
-  );
-};
-
-HierarchyFolderRow.propTypes = {
-  childCount: number.isRequired,
-  children: oneOfType([node, arrayOf(node)]),
-  open: bool.isRequired
-};
+HierarchyFolderRow.propTypes = FolderRow.propTypes;
 
 HierarchyFolderRow.defaultProps = {
-  children: null
+  ...FolderRow.defaultProps,
+  name: faker.commerce.department()
 };
