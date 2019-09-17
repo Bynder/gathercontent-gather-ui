0db2a33315bef9aae17223e8f2f08c11a0f45bca
David Hollins <davidrobertmeek@googlemail.com>
2019-09-17 13:34:53 +0100

    Adding buttonLabel to editabletextwrapper

--- stories/components/EditableTextWrapper.js
+++ stories/components/EditableTextWrapper.js
@@ -4,36 +4,48 @@ import { action } from '@storybook/addon-actions';
 import EditableTextWrapper from '../../lib/EditableTextWrapper';
 import StoryItem from '../styleguide/StoryItem';
 
-const longText = 'Original text thats quite long and may well go over a single line, so its much safer to set the multiline prop, that way we render an ExpandingTextArea component instead of a normal text input';
+const longText =
+  'Original text thats quite long and may well go over a single line, so its much safer to set the multiline prop, that way we render an ExpandingTextArea component instead of a normal text input';
 
-storiesOf('Components', module)
-  .add('EditableTextWrapper', () => (
-    <div>
-      <StoryItem
-        title="EditableTextWrapper"
-        description="Wraps text in an editable wrapper"
+storiesOf('Components', module).add('EditableTextWrapper', () => (
+  <div>
+    <StoryItem
+      title="EditableTextWrapper"
+      description="Wraps text in an editable wrapper"
+    >
+      <EditableTextWrapper
+        buttonLabel="Editable text"
+        value="Original text"
+        onChange={v => action('renamed to: ')(v)}
       >
-        <EditableTextWrapper value="Original text" onChange={v => action('renamed to: ')(v)}>
-          <h1>Original text</h1>
-        </EditableTextWrapper>
-      </StoryItem>
+        <h1>Original text</h1>
+      </EditableTextWrapper>
+    </StoryItem>
 
-      <StoryItem
-        title="EditableTextWrapper pencilEditOnly"
-        description="An EditableTextWrapper where the edit state can only be activated by the pencil button"
+    <StoryItem
+      title="EditableTextWrapper pencilEditOnly"
+      description="An EditableTextWrapper where the edit state can only be activated by the pencil button"
+    >
+      <EditableTextWrapper
+        value="Original text"
+        onChange={v => action('renamed to: ')(v)}
+        pencilEditOnly
       >
-        <EditableTextWrapper value="Original text" onChange={v => action('renamed to: ')(v)} pencilEditOnly>
-          <h1>Original text</h1>
-        </EditableTextWrapper>
-      </StoryItem>
+        <h1>Original text</h1>
+      </EditableTextWrapper>
+    </StoryItem>
 
-      <StoryItem
-        title="EditableTextWrapper multiline"
-        description="An EditableTextWrapper that displays an ExpandingTextArea when in the edit state"
+    <StoryItem
+      title="EditableTextWrapper multiline"
+      description="An EditableTextWrapper that displays an ExpandingTextArea when in the edit state"
+    >
+      <EditableTextWrapper
+        value={longText}
+        onChange={v => action('renamed to: ')(v)}
+        multiline
       >
-        <EditableTextWrapper value={longText} onChange={v => action('renamed to: ')(v)} multiline>
-          <h1>{longText}</h1>
-        </EditableTextWrapper>
-      </StoryItem>
-    </div>
-  ));
+        <h1>{longText}</h1>
+      </EditableTextWrapper>
+    </StoryItem>
+  </div>
+));
