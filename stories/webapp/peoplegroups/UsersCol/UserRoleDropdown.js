3d123864e8ad8bd5587812b3533c16d9ba19e9f1
Amee Morris <amee@live.co.uk>
2019-10-09 09:36:04 +0100

    feedback v3

--- stories/webapp/peoplegroups/UsersCol/UserRoleDropdown.js
+++ stories/webapp/peoplegroups/UsersCol/UserRoleDropdown.js
@@ -6,17 +6,17 @@ const UserRoleDropdown = ({ user, roles }) => (
   <Dropdown id={`user-role-dropown-${user.id}`}>
     <Dropdown.Trigger blueOnActive triggerClassName="typo-size-slight">
       {user.role} <Icon name="down" className="h-vertical-align-middle" />
-      <Dropdown.Content
-        collapse
-        className="h-padding-top-half h-padding-bottom-half"
-      >
-        {roles.map(role => (
-          <Dropdown.Action key={role.id} action={() => {}}>
-            {role.name}
-          </Dropdown.Action>
-        ))}
-      </Dropdown.Content>
     </Dropdown.Trigger>
+    <Dropdown.Content
+      collapse
+      className="h-padding-top-half h-padding-bottom-half"
+    >
+      {roles.map(role => (
+        <Dropdown.Action key={role.id} action={() => {}}>
+          {role.name}
+        </Dropdown.Action>
+      ))}
+    </Dropdown.Content>
   </Dropdown>
 );
 
