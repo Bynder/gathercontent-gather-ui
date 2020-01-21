a143a2eeb2594a15bc7081864bbb5efcf94bca3c
Alex Smith <altaudiolabs@gmail.com>
2020-01-21 17:00:49 +0000

    Adds basic ItemInput

--- stories/index.js
+++ stories/index.js
@@ -65,3 +65,4 @@ import Selection from '../lib/SelectionProvider/stories/SelectionStory';
 import Hierarchy from './webapp/hierarchy/HierarchyStory';
 import ApprovedLabel from '../lib/ApprovedLabel/stories/ApprovedLabelStory';
 import PeopleAndGroups from './webapp/peoplegroups/PeopleGroupsStory';
+import { ItemInputStory } from '../lib/ItemInput/stories/story'
