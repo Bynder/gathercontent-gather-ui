90422789bea6cbdeb2a3e9b40b203e6a99d18781
Alex Smith <altaudiolabs@gmail.com>
2020-02-04 15:31:06 +0000

    Adds spaceBetween prop to ModalFooter

--- stories/components/Modals.js
+++ stories/components/Modals.js
@@ -426,5 +426,54 @@ storiesOf('Components', module).add('Modals', () => (
         </Modal.Container>
       </ModalTrigger>
     </StoryItem>
+
+    <StoryItem
+      title="Modal Footer with Space Between Modifier"
+      description="Modal footer with the items set to be to be spaced out"
+    >
+      <ModalTrigger>
+        <Modal.Container>
+          <Modal.Header>Hello</Modal.Header>
+          <Modal.Body className="has-columns">
+            <Row>
+              <Col
+                className="modal__column-wrapper modal__column-wrapper--highlight"
+                xs={6}
+              >
+                <div className="modal__column">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident
+                </div>
+              </Col>
+              <Col className="modal__column-wrapper" xs={6}>
+                <div className="modal__column">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident
+                </div>
+              </Col>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer spaceBetween>
+            <Button types={['link']} clickHandler={action('clickHandler')}>
+              Cancel
+            </Button>
+            <Button types={['primary']} clickHandler={action('clickHandler')}>
+              Confirm
+            </Button>
+          </Modal.Footer>
+        </Modal.Container>
+      </ModalTrigger>
+    </StoryItem>
+
   </div>
 ));
