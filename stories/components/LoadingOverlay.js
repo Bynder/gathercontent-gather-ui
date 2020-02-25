8053f7b3ee34603f3fb18605c7716815e01531a8
harperllama <kyle@kyleshrives.co.uk>
2020-02-25 09:55:06 +0000

    feedback

--- stories/components/LoadingOverlay.js
+++ stories/components/LoadingOverlay.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text } from '@storybook/addon-knobs';
+import { number } from '@storybook/addon-knobs';
 import { LoadingOverlay } from '../../lib';
 import StoryItem from '../styleguide/StoryItem';
 
 storiesOf('Components', module).add('Loading Overlay', () => {
-  const percentageText = text('Percentage text', '50%');
+  const percentageLoaded = number('Loaded percentage', 50);
 
   return (
-    <div>
-      <StoryItem
-        title="Loading Overlay Component"
-        description=""
-      >
-        <LoadingOverlay
-          percentageText={percentageText}
-          fixed
-        />
-      </StoryItem>
-    </div>
+    <StoryItem
+      title="Loading Overlay Component"
+      description=""
+    >
+      <LoadingOverlay
+        percentageLoaded={percentageLoaded}
+        fixed
+      />
+    </StoryItem>
   );
 });
