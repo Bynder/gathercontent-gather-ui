c419d590bb047dcbf762dff8c24b6e4dc1645be4
David Hollins <davidrobertmeek@googlemail.com>
2020-04-28 13:01:31 +0100

    PR feedback

--- tailwind.config.js
+++ tailwind.config.js
@@ -91,13 +91,46 @@ module.exports = {
       }
     },
     maxHeight: {
-      '32': '8rem'
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '5': '1.25rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '10': ' 2.5rem',
+      '12': ' 3rem',
+      '16': ' 4rem',
+      '20': ' 5rem',
+      '24': ' 6rem',
+      '32': ' 8rem',
+      '40': ' 10rem',
+      '48': ' 12rem',
+      '56': ' 14rem',
+      '64': ' 16rem',
+      auto: 'auto',
+      full: '100%',
+      screen: '100vh'
+    },
+    gradients: {
+      'blur-white-bottom': [
+        'rgba(255, 255, 255, 0) 0%',
+        'rgba(253, 253, 253, 0.5) 50%',
+        '#fff 100%'
+      ],
+      'blur-grey-bottom': [
+        'rgba(255, 255, 255, 0) 0%',
+        'rgba(253, 253, 253, 0.5) 50%',
+        '#f0f2f5 100%'
+      ]
     }
   },
   variants: {
-    padding: ['last']
+    padding: ['last'],
+    gradients: ['responsive', 'hover']
   },
-  plugins: [],
+  plugins: [require('tailwindcss-plugins/gradients')],
   corePlugins: {
     preflight: false,
     container: false
