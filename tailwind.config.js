280172116c0ee8a86199162effb3813b185251c1
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2020-05-08 16:42:33 +0100

    Add RepeatableSettings component to field

--- tailwind.config.js
+++ tailwind.config.js
@@ -111,6 +111,9 @@ module.exports = {
           '30': '#3E4D5C',
           '20': '#29333D'
         }
+      },
+      transitionTimingFunction: {
+        'animation-curve': 'cubic-bezier(0.68, -0.55, 0.265, 1.55)',
       }
     },
     maxHeight: {
@@ -178,7 +181,8 @@ module.exports = {
     padding: ['last'],
     gradients: ['responsive', 'hover'],
     backgroundColor: ['responsive', 'hover', 'focus', 'active'],
-    boxShadow: ['responsive', 'hover', 'focus', 'active']
+    boxShadow: ['responsive', 'hover', 'focus', 'active'],
+    opacity: ['group-hover']
   },
   plugins: [require('tailwindcss-plugins/gradients')],
   corePlugins: {
