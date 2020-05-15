9b1c89f20e20115dfd4c57580da0cf3b129d72ca
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2020-05-15 21:24:22 +0100

    Fix Tertiary Danger button to use borders rather than box shadow hacks

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,7 +33,7 @@ module.exports = {
       'blue-focus-md': `0px 0px 0px 3px ${blue80}`,
       'blue-focus-sm': `0px 0px 0px 3px ${blue80}`,
       'red-focus-md': `0px 0px 0px 3px ${red80}`,
-      'red-focus-sm': `0px 0px 0px 1px ${redPrimary}, 0px 0px 0px 4px ${red80}`,
+      'red-focus-sm': `0px 0px 0px 3px ${red80}`,
       'neutral-focus-sm': `0px 0px 0px 3px ${neutral80}`,
 
       none: 'none'
