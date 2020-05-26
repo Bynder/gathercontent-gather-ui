e2b5ddfb3ad7d52cabac754d8fdeeeda2b011792
David Hollins <davidrobertmeek@googlemail.com>
2020-05-26 16:24:28 +0100

    Creating a few testers for peaks

--- stories/components/Tabs.js
+++ stories/components/Tabs.js
@@ -1,99 +1,135 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { number } from '@storybook/addon-knobs';
+import faker from 'faker';
 import { Tabs } from '../../lib';
 import StoryItem from '../styleguide/StoryItem';
-import Icon from '../../lib/Icon';
 
-const options = [{
-  name: 'Edit',
-  action: () => {},
-}, {
-  name: 'Delete',
-  action: () => {},
-}, {
-  name: 'Move Left',
-  action: () => {},
-}, {
-  name: 'Move Right',
-  action: () => {},
-}];
+// eslint-disable-next-line react/prop-types
+function NewTabButton({ className }) {
+  return (
+    <div
+      className={`self-center border-neutral-90 border border-solid px-2 font-medium hover:bg-neutral-90 break-all h-10 flex items-center justify-center bg-white text-blue-primary cursor-pointer ${className}`}
+    >
+      + New Tab
+    </div>
+  );
+}
 
-storiesOf('Components', module)
-  .add('Tabs', () => {
-    return (
-      <div>
-        <StoryItem
-          title="Tabs"
-          description="Components that renders a both the tabs container and individual tabs."
-        >
-          <Tabs activeTabId="content" onTabChange={(id) => console.log(`tab changed to ${id}`)}>
-            <Tabs.Item id="content">Content</Tabs.Item>
-            <Tabs.Item id="meta">Meta</Tabs.Item>
-            <Tabs.Item id="english">English</Tabs.Item>
-            <Tabs.Item id="french">French</Tabs.Item>
-            <Tabs.Item id="german">German</Tabs.Item>
-          </Tabs>
-        </StoryItem>
+// eslint-disable-next-line react/prop-types
+function StretchTabs({ tabs }) {
+  const [activeTab, setActiveTab] = useState(0);
 
-        <StoryItem
-          title="Tabs: Outlined"
-          description="Tabs can be outlined, which is helpful when the text inside are editable."
-        >
-          <Tabs
-            activeTabId="meta"
-            onTabChange={(id) => console.log(`tab changed to ${id}`)}
-            editable
-          >
-            <Tabs.Item id="content">Content</Tabs.Item>
-            <Tabs.Item id="meta">Meta</Tabs.Item>
-            <Tabs.Item id="english">English</Tabs.Item>
-            <Tabs.Item id="french">French</Tabs.Item>
-            <Tabs.Item id="german">German</Tabs.Item>
-          </Tabs>
-        </StoryItem>
+  return (
+    <StoryItem title="Tabs" description="Full amount of tabs">
+      <Tabs className="flex flex-wrap w-full">
+        {tabs.map((t, index) => (
+          <Tabs.Item isActive={activeTab === index}>
+            {(className, ref) => (
+              <div
+                className={`no-underline text-center ${className}`}
+                style={{
+                  'white-space': 'nowrap',
+                  overflow: 'hidden',
+                  'text-overflow': 'ellipsis',
+                  'flex-basis': 0,
+                  'min-width': '10%',
+                  cursor: 'pointer'
+                }}
+                ref={ref}
+                onClick={() => setActiveTab(index)}
+              >
+                {t.name}
+              </div>
+            )}
+          </Tabs.Item>
+        ))}
+        <NewTabButton className="ml-auto" />
+      </Tabs>
+    </StoryItem>
+  );
+}
 
-        <StoryItem
-          title="Tabs: With Options"
-          description="Tabs can show options per tab in a drop down."
-        >
-          <Tabs activeTabId="english" onTabChange={(id) => console.log(`tab changed to ${id}`)}>
-            <Tabs.Item id="content" options={options}>Content</Tabs.Item>
-            <Tabs.Item id="meta" options={options}>Meta</Tabs.Item>
-            <Tabs.Item id="english" options={options}>English</Tabs.Item>
-            <Tabs.Item id="french" options={options}>French</Tabs.Item>
-            <Tabs.Item id="german" options={options}>German</Tabs.Item>
-          </Tabs>
-        </StoryItem>
+// eslint-disable-next-line react/prop-types
+function FixedTabs({ tabs }) {
+  const [activeTab, setActiveTab] = useState(0);
 
-        <StoryItem
-          title="Tabs: Buttons"
-          description="Tabs can be buttons, which do not change the active tab but instead fire a function."
-        >
-          <Tabs activeTabId="french" onTabChange={(id) => console.log(`tab changed to ${id}`)}>
-            <Tabs.Item id="content">Content</Tabs.Item>
-            <Tabs.Item id="meta">Meta</Tabs.Item>
-            <Tabs.Item id="english">English</Tabs.Item>
-            <Tabs.Item id="french">French</Tabs.Item>
-            <Tabs.Item id="german">German</Tabs.Item>
-            <Tabs.Button onClick={() => console.log('Tab button clicked')}>
-              <Icon name="plus" className="fill-primary-blue" />
-            </Tabs.Button>
-          </Tabs>
-        </StoryItem>
+  return (
+    <Tabs className="flex-wrap w-full">
+      {tabs.map((t, index) => (
+        <Tabs.Item isActive={activeTab === index}>
+          {(className, ref) => (
+            <div
+              className={`no-underline text-center ${className}`}
+              style={{
+                'white-space': 'nowrap',
+                overflow: 'hidden',
+                'text-overflow': 'ellipsis',
+                'flex-basis': 0,
+                'min-width': '10%',
+                cursor: 'pointer'
+              }}
+              ref={ref}
+              onClick={() => setActiveTab(index)}
+            >
+              {t.name}
+            </div>
+          )}
+        </Tabs.Item>
+      ))}
+      <NewTabButton className="ml-auto" />
+    </Tabs>
+  );
+}
 
-        <StoryItem
-          title="Tabs: Forms"
-          description="Tabs can contain form elements, useful when a tab becomes editable."
-        >
-          <Tabs onTabChange={(id) => console.log(`tab changed to ${id}`)} editable>
-            <Tabs.Item id="content">Content</Tabs.Item>
-            <Tabs.Form
-              onSubmit={(value) => console.log(`The value is ${value}`)}
-              onCancel={() => console.log('Run cancel function')}
-            />
-          </Tabs>
-        </StoryItem>
-      </div>
-    );
-  });
+// eslint-disable-next-line react/prop-types
+function VariableTabs({ tabs }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <Tabs className="flex-wrap w-full">
+      {tabs.map((t, index) => (
+        <Tabs.Item isActive={activeTab === index}>
+          {(className, ref) => (
+            <div
+              className={`no-underline text-center ${className}`}
+              style={{
+                'white-space': 'nowrap',
+                overflow: 'hidden',
+                'text-overflow': 'ellipsis',
+                'min-width': '0',
+                cursor: 'pointer'
+              }}
+              ref={ref}
+              onClick={() => setActiveTab(index)}
+            >
+              {t.name}
+            </div>
+          )}
+        </Tabs.Item>
+      ))}
+      <NewTabButton className="ml-auto" />
+    </Tabs>
+  );
+}
+
+storiesOf('Components', module).add('Tabs', () => {
+  const tabsNumber = number('Total number of tabs', 16);
+  const tabs = [...Array(tabsNumber).keys()].map(() => ({
+    name: faker.commerce.productName()
+  }));
+
+  return (
+    <Fragment>
+      <StoryItem title="Stretch">
+        <StretchTabs tabs={tabs} />
+      </StoryItem>
+      <StoryItem title="Fixed">
+        <FixedTabs tabs={tabs} />
+      </StoryItem>
+      <StoryItem title="Variable">
+        <VariableTabs tabs={tabs} />
+      </StoryItem>
+    </Fragment>
+  );
+});
