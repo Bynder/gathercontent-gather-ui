1fd5279a9739560c1e9e38609f84331b3bfd7c32
David Hollins <davidrobertmeek@googlemail.com>
2020-05-28 14:34:08 +0100

    Refactoring the styles of tabs

--- stories/components/Tabs.js
+++ stories/components/Tabs.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
 import { storiesOf } from '@storybook/react';
 import { number } from '@storybook/addon-knobs';
@@ -10,60 +10,52 @@ import Icon from '../../lib/Icon';
 function StretchTabsWithButtonAtStart({ tabs }) {
   const [activeTab, setActiveTab] = useState(0);
 
-  const tabsClassName = cx('flex flex-wrap w-full relative', {
-    'overflow-y-hidden': tabs.length <= 24,
-    'overflow-y-scroll': tabs.length > 24
-  });
-
   return (
-    <div className="relative">
-      <Tabs
-        className={tabsClassName}
-        style={{
-          'max-height': '120px'
-        }}
-      >
-        {tabs.map((t, index) => (
-          <Tabs.Item isActive={activeTab === index}>
-            {(className, ref) => (
-              <div
-                className={`flex group no-underline justify-center text-center flex-grow cursor-pointer font-semi-bold relative ${className}`}
-                style={{
-                  'flex-basis': 0,
-                  'min-width': tabs.length >= 8 ? '12.5%' : 0,
-                  'max-width': tabs.length >= 8 ? '12.5%' : 'initial'
-                }}
-                ref={ref}
-                onClick={() => setActiveTab(index)}
-              >
-                <div className="group overflow-hidden cursor-pointer w-full relative overflow-hidden whitespace-no-wrap">
-                  {t.name}
-                </div>
-                <div className="flex items-center absolute right-0 top-0 bottom-0 w-8 pr-2">
-                  <span
-                    className={`w-full h-full group-hover:bg-blur-grey-90-right ${
-                      activeTab === index
-                        ? 'bg-blur-white-right'
-                        : 'bg-blur-grey-95-right'
-                    }`}
-                  />
-                  <Icon
-                    className="group-hover:block group-hover:bg-neutral-90 hidden"
-                    name="cog"
-                  />
-                </div>
+    <Tabs
+      actions={
+        <ButtonSecondary
+          className="rounded-b border-t-0 rounded-t-none"
+          onClick={() => {}}
+        >
+          + New Tab
+        </ButtonSecondary>
+      }
+      tabs={tabs}
+      className="bg-neutral-95 border-neutral-90 border-t border-b-0 border-l-0 border-r border-solid"
+    >
+      {tabs.map((t, index) => (
+        <Tabs.Base isActive={activeTab === index} tabsLength={tabs.length}>
+          {(className, style) => (
+            <button
+              type="button"
+              className={`relative border-neutral-90 border border-solid font-medium hover:bg-neutral-90 break-all h-10 items-center border-t-0 border-r-0 px-2 font-semi-bold ${className}`}
+              onClick={e => {
+                e.preventDefault();
+                setActiveTab(index);
+              }}
+              style={style}
+            >
+              <div className="group overflow-hidden w-full relative overflow-hidden whitespace-no-wrap">
+                {t.name}
               </div>
-            )}
-          </Tabs.Item>
-        ))}
-      </Tabs>
-      <ButtonSecondary
-        className="absolute rounded-b border-t-0 rounded-t-none right-0 mr-4"
-        onClick={() => {}}
-      >
-        + New Tab
-      </ButtonSecondary>
-    </div>
+              <div className="flex items-center absolute right-0 top-0 bottom-0 w-8 pr-2">
+                <span
+                  className={`w-full h-full group-hover:bg-blur-grey-90-right ${
+                    activeTab === index
+                      ? 'bg-blur-white-right'
+                      : 'bg-blur-grey-95-right'
+                  }`}
+                />
+                <Icon
+                  className="group-hover:block group-hover:bg-neutral-90 hidden"
+                  name="cog"
+                />
+              </div>
+            </button>
+          )}
+        </Tabs.Base>
+      ))}
+    </Tabs>
   );
 }
 
