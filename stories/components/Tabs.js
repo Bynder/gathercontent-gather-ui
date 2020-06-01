b0cc694b995c861d50818bbd1fd2bfaaca420d13
David Hollins <davidrobertmeek@googlemail.com>
2020-06-01 11:46:00 +0100

    Refactoring the new tabs to use better compound components

--- stories/components/Tabs.js
+++ stories/components/Tabs.js
@@ -10,54 +10,56 @@ function TabsStory({ tabs }) {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <Tabs
-      actions={
+    <Tabs>
+      <Tabs.Group
+        tabsLength={tabs.length}
+        className="bg-neutral-95 border-neutral-90 border-t border-b-0 border-l-0 border-r border-solid"
+      >
+        {tabs.map((t, index) => (
+          <Tabs.Base tabsLength={tabs.length}>
+            {(className, style) => (
+              <button
+                type="button"
+                className={`relative border-neutral-90 border border-solid font-medium hover:bg-neutral-90 break-all h-10 items-center border-t-0 border-r-0 px-2 font-semi-bold ${className} ${
+                  activeTab === index
+                    ? 'bg-white text-blue-primary'
+                    : 'bg-neutral-95 text-neutral-primary'
+                }`}
+                onClick={e => {
+                  e.preventDefault();
+                  setActiveTab(index);
+                }}
+                style={style}
+              >
+                <div className="group overflow-hidden w-full relative overflow-hidden whitespace-no-wrap">
+                  {t.name}
+                </div>
+                <div className="flex items-center absolute right-0 top-0 bottom-0 w-8 pr-2">
+                  <span
+                    className={`w-full h-full group-hover:bg-blur-grey-90-right ${
+                      activeTab === index
+                        ? 'bg-blur-white-right'
+                        : 'bg-blur-grey-95-right'
+                    }`}
+                  />
+                  <Icon
+                    className="group-hover:block group-hover:bg-neutral-90 hidden"
+                    name="cog"
+                  />
+                </div>
+              </button>
+            )}
+          </Tabs.Base>
+        ))}
+      </Tabs.Group>
+      <Tabs.ActionGroup>
         <ButtonSecondary
           className="rounded-b border-t-0 rounded-t-none"
           onClick={() => {}}
         >
           + New Tab
         </ButtonSecondary>
-      }
-      tabs={tabs}
-      className="bg-neutral-95 border-neutral-90 border-t border-b-0 border-l-0 border-r border-solid"
-    >
-      {tabs.map((t, index) => (
-        <Tabs.Base tabsLength={tabs.length}>
-          {(className, style) => (
-            <button
-              type="button"
-              className={`relative border-neutral-90 border border-solid font-medium hover:bg-neutral-90 break-all h-10 items-center border-t-0 border-r-0 px-2 font-semi-bold ${className} ${
-                activeTab === index
-                  ? 'bg-white text-blue-primary'
-                  : 'bg-neutral-95 text-neutral-primary'
-              }`}
-              onClick={e => {
-                e.preventDefault();
-                setActiveTab(index);
-              }}
-              style={style}
-            >
-              <div className="group overflow-hidden w-full relative overflow-hidden whitespace-no-wrap">
-                {t.name}
-              </div>
-              <div className="flex items-center absolute right-0 top-0 bottom-0 w-8 pr-2">
-                <span
-                  className={`w-full h-full group-hover:bg-blur-grey-90-right ${
-                    activeTab === index
-                      ? 'bg-blur-white-right'
-                      : 'bg-blur-grey-95-right'
-                  }`}
-                />
-                <Icon
-                  className="group-hover:block group-hover:bg-neutral-90 hidden"
-                  name="cog"
-                />
-              </div>
-            </button>
-          )}
-        </Tabs.Base>
-      ))}
+      </Tabs.ActionGroup>
     </Tabs>
   );
 }
