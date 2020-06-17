a85d0e9f4d31ab21a957d50936b2e2c455bc93a2
harperllama <kyle@kyleshrives.co.uk>
2020-06-17 16:22:58 +0100

    add rows

--- stories/components/Tabs.js
+++ stories/components/Tabs.js
@@ -9,47 +9,56 @@ import Dropdown from "../../lib/Dropdown";
 // eslint-disable-next-line react/prop-types
 function TabsStory({ tabs, dragSide, dragIndex }) {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
+  const numberOfRows = Math.ceil(tabs.length / 8);
+  const rows = [...new Array(numberOfRows)].map((i, index) => {
+    const start = index * 8;
+    return tabs.slice(start, start + 8);
+  })
 
   return (
     <Tabs tabs={tabs} activeTabId={activeTabId}>
       <Tabs.Group>
-        {tabs.map((t, index) => (
-          <Tabs.TabBase id={t.id} index={index}>
-            {(wrapperClasses, buttonClasses) => (
-              <div className={wrapperClasses}>
-                <button
-                  type="button"
-                  className={buttonClasses}
-                  onClick={e => {
-                    e.preventDefault();
-                    setActiveTabId(t.id);
-                  }}
-                  title={t.name}
-                >
-                  <Tabs.TabName>
-                    {t.name}
-                  </Tabs.TabName>
-                </button>
+        {rows.map((r, i) => (
+          <Tabs.Row index={i}>
+            {r.map((t, index) => (
+              <Tabs.TabBase id={t.id} index={index}>
+                {(wrapperClasses, buttonClasses) => (
+                  <div className={wrapperClasses}>
+                    <button
+                      type="button"
+                      className={buttonClasses}
+                      onClick={e => {
+                        e.preventDefault();
+                        setActiveTabId(t.id);
+                      }}
+                      title={t.name}
+                    >
+                      <Tabs.TabName>
+                        {t.name}
+                      </Tabs.TabName>
+                    </button>
 
-                <Tabs.TabAside id={t.id}>
-                  <Tabs.TabOptions id={t.id}>
-                    <Dropdown.ActionGroup className="text-neutral-20 font-normal">
-                      <Dropdown.Action
-                        className="text-neutral-20 weight-roman"
-                        action={() => {}}
-                      >
-                        Action text
-                      </Dropdown.Action>
-                    </Dropdown.ActionGroup>
-                  </Tabs.TabOptions>
-                </Tabs.TabAside>
+                    <Tabs.TabAside id={t.id}>
+                      <Tabs.TabOptions id={t.id}>
+                        <Dropdown.ActionGroup className="text-neutral-20 font-normal">
+                          <Dropdown.Action
+                            className="text-neutral-20 weight-roman"
+                            action={() => {}}
+                          >
+                            Action text
+                          </Dropdown.Action>
+                        </Dropdown.ActionGroup>
+                      </Tabs.TabOptions>
+                    </Tabs.TabAside>
 
-                {dragSide && dragIndex === index && (
-                  <Tabs.TabDragLine side={dragSide} />
+                    {dragSide && dragIndex === index && (
+                      <Tabs.TabDragLine side={dragSide} />
+                    )}
+                  </div>
                 )}
-              </div>
-            )}
-          </Tabs.TabBase>
+              </Tabs.TabBase>
+            ))}
+          </Tabs.Row>
         ))}
       </Tabs.Group>
 
@@ -66,7 +75,7 @@ function TabsStory({ tabs, dragSide, dragIndex }) {
 }
 
 storiesOf('Components', module).add('Tabs', () => {
-  const tabsNumber = number('Total number of tabs', 10);
+  const tabsNumber = number('Total number of tabs', 26);
   const groupId = 'Drag And Drop';
 
   const label = 'Drag Line';
