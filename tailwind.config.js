e9f343e0adaad9eff951379fcef37f7a85e9838a
Alex Smith <altaudiolabs@gmail.com>
2020-07-07 15:17:23 +0100

    Adds ButtonTertiary active border styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,8 +45,8 @@ module.exports = {
 
       'button-secondary-focus': `0px 0px 0px 3px ${blue80}`,
 
-      'button-tertiary-focus': `0px 0px 0px 1px ${neutral20}, 0px 0px 0px 3px ${neutral80}`,
-      'button-tertiary-danger-focus': `0px 0px 0px 1px ${redPrimary}, 0px 0px 0px 3px ${red80}`,
+      'button-tertiary-focus': `0px 0px 0px 3px ${neutral80}`,
+      'button-tertiary-danger-focus': `0px 0px 0px 3px ${red80}`,
 
       'button-icon-focus': `0px 0px 0px 1px ${bluePrimary}, 0px 0px 0px 3px ${blue80}`,
       'button-icon-danger-focus': `0px 0px 0px 1px ${redPrimary}, 0px 0px 0px 3px ${red80}`,
