ec95bf44b89d27cdfa062ddeb8dda2d0471ca159
David Hollins <davidhollins06@gmail.com>
2021-01-21 15:11:34 +0000

    Creating a cancel button module

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import 'lib/ButtonNew/stories/ButtonLink/ButtonLinkStory';
 import 'lib/ButtonNew/stories/ButtonAvatar/ButtonAvatarStory';
 
 import 'lib/src/modules/loader/LoaderStory';
+import 'lib/src/modules/cancelButton/CancelButtonStory';
 import 'lib/src/modules/inventoryItem/InventoryItemStory';
 import 'lib/src/modules/selectionControls/SelectionControlsStory';
 import 'lib/src/modules/renameInput/RenameInputStory';
