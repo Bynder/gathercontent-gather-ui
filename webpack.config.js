4b3d60a9643c3855a5775929bdd759d3bb30ce3d
David Hollins <davidhollins06@gmail.com>
2021-07-28 12:54:09 +0100

    Switching to webpack out of desperation

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+
+module.exports = {
+  plugins: [
+    new CircularDependencyPlugin({
+      // exclude detection of files based on a RegExp
+      exclude: /a\.js|node_modules/,
+      // add errors to webpack instead of warnings
+      failOnError: false,
+      // set the current working directory for displaying module paths
+      cwd: process.cwd(),
+    })
+  ],
+  entry: './lib/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: [{
+          loader: 'file-loader'
+        }],
+        include: [path.resolve(__dirname, '../node_modules/font-awesome'), path.resolve(__dirname, './IBM-Plex')]
+      },
+      {
+        test: /\.s?css$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            plugins: function () {
+              return [
+                require('tailwindcss'),
+                require('autoprefixer'),
+                require('pixrem'),
+              ]
+            }
+          }
+        }, {
+          loader: 'sass-loader',
+          options: {
+            implementation: require("sass")
+          }
+        }],
+        include: [path.resolve(__dirname, '../'), path.resolve(__dirname, '../node_modules/font-awesome')]
+      },
+      {
+        test: /.less$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'less-loader'
+        }],
+        include: path.resolve(__dirname, '../')
+      },
+      {
+        test: /\.svg$/,
+        use: [{
+          loader: 'raw-loader'
+        }]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+};
