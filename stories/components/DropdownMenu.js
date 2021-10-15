14686b52221fe6787d26bd628a277ad547cd963c
Kyle Harper <kyleharperllama@gmail.com>
2021-10-15 15:20:34 +0100

    delete deprecated components

--- stories/components/DropdownMenu.js
+++ stories/components/DropdownMenu.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { DropdownMenu } from '../../lib/DropdownMenu/';
-import SearchDropdown from '../../lib/SearchDropdown';
-import FontAwesomeIcon from '../../lib/FontAwesomeIcon';
+import { DropdownMenu } from 'lib/DropdownMenu';
+import SearchDropdown from 'lib/SearchDropdown';
 import StoryItem from '../styleguide/StoryItem';
 import * as assets from '../../assets/data';
 
@@ -48,19 +46,6 @@ storiesOf('Components', module)
           description="A link that opens a dropdown.">
           <DropdownMenu type="link" caret shouldDisplay items={assets.getDropdownItemsWithSeparator()}>
             Actions
-          </DropdownMenu>
-        </StoryItem>
-
-        <StoryItem
-          title="Dropdown link"
-          description="A dropdown menu from a link style, without a caret">
-          <DropdownMenu
-            caret={false}
-            type="link"
-            direction="up"
-            shouldDisplay
-            items={assets.getDropdownItems()}>
-              <FontAwesomeIcon style={{ fontSize: '18px' }} name="fa-cog"/>
           </DropdownMenu>
         </StoryItem>
 
