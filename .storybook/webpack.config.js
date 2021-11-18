07624765f8ae6c3bd66b3b8a0bd77e6e5a7adaae
Amee Morris <amee@live.co.uk>
2021-11-18 15:35:31 +0000

    updating tailwind to the latest version

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
-const Fiber = require('fibers');
 
 module.exports = {
   plugins: [
@@ -10,61 +9,72 @@ module.exports = {
       // add errors to webpack instead of warnings
       failOnError: false,
       // set the current working directory for displaying module paths
-      cwd: process.cwd(),
+      cwd: process.cwd()
     })
   ],
   module: {
     rules: [
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: [{
-          loader: 'file-loader'
-        }],
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ],
         include: [path.resolve(__dirname, './IBM-Plex')]
       },
       {
         test: /\.s?css$/,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader'
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            plugins: function () {
-              return [
-                require('tailwindcss'),
-                require('autoprefixer'),
-                require('pixrem'),
-              ]
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['tailwindcss', 'autoprefixer', 'pixrem']
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                fiber: require('fibers')
+              }
             }
           }
-        }, {
-          loader: 'sass-loader',
-          options: {
-            implementation: require("sass"),
-            fiber: Fiber
-          }
-        }],
+        ],
         include: [path.resolve(__dirname, '../')]
       },
       {
         test: /.less$/,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader'
-        }, {
-          loader: 'less-loader'
-        }],
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader'
+          }
+        ],
         include: path.resolve(__dirname, '../')
       },
       {
         test: /\.svg$/,
-        use: [{
-          loader: 'raw-loader'
-        }]
+        use: [
+          {
+            loader: 'raw-loader'
+          }
+        ]
       }
     ]
-  },
+  }
 };
