9a8446759d90c3c19be01d71b52047fe46cce3d8
Amee Morris <amee@live.co.uk>
2023-05-10 12:58:39 +0100

    upgrade to storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,56 +1,26 @@
 const path = require('path');
-
 module.exports = {
   stories: ['../lib/**/*.stories.js', '../stories/**/*.stories.js'],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-controls',
-    '@storybook/addon-notes',
-    '@storybook/addon-a11y'
-  ],
-  webpackFinal: async (config, { configType }) => {
-    // storybook doesnt play well with postcss8 which latest tailwind requires
-    // if we have two css rules it errors, so we need to strip out storybook's
-    const rules = config.module
-      ? config.module.rules.filter(rule => {
-          const storybooksCssTest = new RegExp(/\.css$/);
-          return rule && rule.test.toString() !== storybooksCssTest.toString();
-        })
-      : [];
-    return {
-      ...config,
-      module: {
-        ...config.module,
-        rules: [
-          ...rules,
-          {
-            test: /\.s?css$/,
-            use: [
-              {
-                loader: 'style-loader'
-              },
-              {
-                loader: 'css-loader'
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  postcssOptions: {
-                    plugins: ['tailwindcss', 'autoprefixer', 'pixrem']
-                  }
-                }
-              },
-              {
-                loader: 'sass-loader',
-                options: {
-                  implementation: require('sass')
-                }
-              }
-            ],
-            include: [path.resolve(__dirname, '../')]
-          }
-        ]
+    '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require('sass')
+        }
       }
-    };
+    }
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
 };
