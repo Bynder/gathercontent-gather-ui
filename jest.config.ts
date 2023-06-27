dce72d9a502597cd0b900cf34e860aeaf0003d68
Amee Morris <amee@live.co.uk>
2023-06-27 16:26:52 +0100

    fix test paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,9 @@
 module.exports = {
   verbose: true,
-  testMatch: ['**/tests/**/*.js?(x)', '**/tests/**/*.spec.js', '**/specs/**/*.spec.js'],
-  testPathIgnorePatterns: ['tests/setup.js', 'dist']
+  testMatch: [
+    "**/tests/**/*.ts?(x)",
+    "**/tests/**/*.spec.tsx",
+    "**/specs/**/*.spec.tx",
+  ],
+  testPathIgnorePatterns: ["tests/setup.js", "dist"],
 };
