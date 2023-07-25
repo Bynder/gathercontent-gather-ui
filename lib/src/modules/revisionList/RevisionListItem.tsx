ba9d4e893bf19e2d60b712f8884fbcaaa71e8bc7
Amee Morris <amee@live.co.uk>
2023-07-25 11:34:26 +0100

    add nested list example to story and styles

--- lib/src/modules/revisionList/RevisionListItem.tsx
+++ lib/src/modules/revisionList/RevisionListItem.tsx
@@ -1,13 +1,25 @@
+import cx from "classnames";
 import React, { HTMLAttributes } from "react";
+
+interface Props extends HTMLAttributes<HTMLLIElement> {
+  leftContent?: React.ReactNode;
+  active?: boolean;
+}
 
 export function RevisionListItem({
   children,
   className = "",
+  leftContent,
+  active,
   ...rest
-}: HTMLAttributes<HTMLLIElement>) {
+}: Props) {
+  const classes = cx(className, "revision-list-item", {
+    "revision-list-item-active": active,
+  });
   return (
     <li className={`${className} revision-list-item`} {...rest}>
-      {children}
+      <div className="revision-list-item-left">{leftContent}</div>
+      <div className="w-full">{children}</div>
     </li>
   );
 }
--- lib/src/modules/revisionList/stories/RevisionsList.stories.tsx
+++ lib/src/modules/revisionList/stories/RevisionsList.stories.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import faker from "faker";
 import { RevisionList as RevisionListComponent } from "../RevisionList";
 import Avatar from "../../../../Avatar";
 import { RevisionListStatus } from "../RevisionListStatus";
+import { ButtonIcon } from "../../Button/ButtonIcon/ButtonIcon";
 
 export default {
   title: "GUI/RevisionList",
@@ -10,26 +11,71 @@ export default {
 };
 
 export function RevisionsList() {
+  const [showNested, setShowNested] = useState(false);
+
+  const onClick = (e) => {
+    e.preventDefault();
+    setShowNested(!showNested);
+  };
+  const LeftContent = (
+    <ButtonIcon
+      name={showNested ? "down" : "chevronRight"}
+      size="sm"
+      onClick={onClick}
+      className="mt-1"
+    />
+  );
   return (
     <>
       <RevisionListComponent.Heading>
         April 13, 2023
       </RevisionListComponent.Heading>
       <RevisionListComponent>
-        <RevisionListComponent.Item>
-          <RevisionListComponent.Timestamp timestamp="6:33 pm">
-            <Avatar url={faker.image.avatar()} />
-          </RevisionListComponent.Timestamp>
-          <RevisionListStatus
-            oldStatusColour="#C2CCD6"
-            statusColour="#F9D006"
-            statusName="Review"
-          />
-          <RevisionListComponent.Note>
-            Hey @angus - the content is ready for review. Feel free to make any
-            necessary changes.
-          </RevisionListComponent.Note>
-        </RevisionListComponent.Item>
+        <a href="#">
+          <RevisionListComponent.Item leftContent={LeftContent}>
+            <RevisionListComponent.Timestamp timestamp="6:33 pm">
+              <Avatar url={faker.image.avatar()} />
+            </RevisionListComponent.Timestamp>
+            <RevisionListStatus
+              oldStatusColour="#C2CCD6"
+              statusColour="#F9D006"
+              statusName="Review"
+            />
+            <RevisionListComponent.Note>
+              Hey @angus - the content is ready for review. Feel free to make
+              any necessary changes.
+            </RevisionListComponent.Note>
+          </RevisionListComponent.Item>
+        </a>
+        {showNested && (
+          <RevisionListComponent>
+            <RevisionListComponent.Item>
+              <RevisionListComponent.Timestamp timestamp="6:33 pm">
+                <Avatar url={faker.image.avatar()} />
+              </RevisionListComponent.Timestamp>
+            </RevisionListComponent.Item>
+            <RevisionListComponent.Item>
+              <RevisionListComponent.Timestamp timestamp="5:33 pm">
+                <Avatar url={faker.image.avatar()} />
+              </RevisionListComponent.Timestamp>
+            </RevisionListComponent.Item>
+            <RevisionListComponent.Item>
+              <RevisionListComponent.Timestamp timestamp="4:33 pm">
+                <Avatar url={faker.image.avatar()} />
+              </RevisionListComponent.Timestamp>
+            </RevisionListComponent.Item>
+          </RevisionListComponent>
+        )}
+      </RevisionListComponent>
+      <RevisionListComponent>
+        <a href="#">
+          <RevisionListComponent.Item>
+            <RevisionListComponent.Timestamp timestamp="6:33 pm">
+              <Avatar url={faker.image.avatar()} />
+            </RevisionListComponent.Timestamp>
+            <RevisionListStatus statusColour="#F9D006" statusName="Review" />
+          </RevisionListComponent.Item>
+        </a>
       </RevisionListComponent>
     </>
   );
