6c8849c83fb63af1a57e54b3d5c0db5e5f9e2c8f
Amee Morris <amee@live.co.uk>
2023-08-30 12:08:54 +0100

    WIP react 18 update - tests all messed up but no peer deps issues

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,5 @@
 import React from "react";
-// @ts-expect-error
-import Enzyme, { shallow, mount } from "enzyme";
-// @ts-expect-error
-import Adapter from "enzyme-adapter-react-16";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 
-Enzyme.configure({ adapter: new Adapter() });
 
-export { React, shallow, mount };
+export { React };
