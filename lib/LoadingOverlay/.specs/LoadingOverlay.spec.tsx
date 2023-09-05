98f1d11286a5902d0aa8504292fabe1622cd2b8a
Amee Morris <amee@live.co.uk>
2023-09-05 11:41:39 +0100

    split up tests a little

--- lib/LoadingOverlay/.specs/LoadingOverlay.spec.tsx
+++ lib/LoadingOverlay/.specs/LoadingOverlay.spec.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import React from "react";
 
@@ -6,30 +6,42 @@ import { render, screen } from "@testing-library/react";
 import LoadingOverlay from "..";
 
 describe("LoadingOverlay", () => {
-  it("can optionally load the svg, text, fixed modifier, and a loading percentage", () => {
-    const loadingText = "Gathering flumps";
-    const loadingPercentage = 25;
+  it("can optionally load the svg", () => {
+    const { rerender } = render(<LoadingOverlay />);
+    expect(screen.getByTitle("Loading"));
+
+    rerender(<LoadingOverlay hideSVG />);
+
+    expect(screen.queryByTitle("Loading")).toBeFalsy();
+  });
+
+  it("can optionally load a fixed modifier", () => {
     const { rerender } = render(<LoadingOverlay />);
     expect(
       screen.getByRole("status").classList.contains("loading-overlay--fixed")
     ).toBeFalsy();
-    expect(screen.getByTitle("Loading"));
+
+    rerender(<LoadingOverlay fixed />);
+
+    expect(
+      screen.getByRole("status").classList.contains("loading-overlay--fixed")
+    ).toBeTruthy();
+  });
+
+  it("can optionally load text and a loading percentage", () => {
+    const loadingText = "Gathering flumps";
+    const loadingPercentage = 25;
+    const { rerender } = render(<LoadingOverlay />);
     expect(screen.queryByText(loadingText)).toBeFalsy();
     expect(screen.queryByText(`${loadingPercentage}%`)).toBeFalsy();
 
     rerender(
       <LoadingOverlay
-        hideSVG
-        fixed
         percentageLoaded={loadingPercentage}
         loadingText={loadingText}
       />
     );
 
-    expect(
-      screen.getByRole("status").classList.contains("loading-overlay--fixed")
-    ).toBeTruthy();
-    expect(screen.queryByTitle("Loading")).toBeFalsy();
     expect(screen.getByText(loadingText));
     expect(screen.getByText(`${loadingPercentage}%`));
   });
