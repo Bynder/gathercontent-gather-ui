fc91d3ce3c8340afac49e9f89f0430d7965e8f27
micmcgrorty <michaelmcgrorty2@gmail.com>
2023-09-05 14:09:29 +0100

    add correct props type to Form

--- lib/src/components/workflowStep/WorkflowStep.stories.tsx
+++ lib/src/components/workflowStep/WorkflowStep.stories.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 // @ts-expect-error TS(2307): Cannot find module 'stories/styleguide/StoryItem' ... Remove this comment to see the full error message
-import StoryItem from "stories/styleguide/StoryItem";
-import { ButtonPrimary, Label, TextForm } from "lib";
-import { WorkflowStep as WorkflowStepComponent } from "./workflowStep";
+import StoryItem from 'stories/styleguide/StoryItem';
+import { ButtonPrimary, Label, TextForm } from 'lib';
+import { WorkflowStep as WorkflowStepComponent } from './workflowStep';
 
 export default {
-  title: "GUI/Workflow Step",
+  title: 'GUI/Workflow Step',
   component: WorkflowStepComponent,
 };
 
@@ -17,7 +17,7 @@ export function WorkflowStep() {
       <StoryItem title="Workflow Step">
         <div
           style={{
-            width: "680px",
+            width: '680px',
           }}
         >
           <WorkflowStepComponent
@@ -33,7 +33,7 @@ export function WorkflowStep() {
               <WorkflowStepComponent.Meta
                 actions={
                   <WorkflowStepComponent.Actions>
-                    {() => "Actions"}
+                    {() => 'Actions'}
                   </WorkflowStepComponent.Actions>
                 }
               >
@@ -45,7 +45,6 @@ export function WorkflowStep() {
                 Creating the initial draft of content following the project
                 style guide and requirements for this item.
               </p>
-              {/* @ts-expect-error */}
               <TextForm className="pt-4 pr-4">
                 <TextForm.Body>
                   <Label htmlFor="test">
@@ -70,7 +69,7 @@ export function WorkflowStep() {
       <StoryItem title="Active Step">
         <div
           style={{
-            width: "680px",
+            width: '680px',
           }}
         >
           <WorkflowStepComponent className="w-full h-full" showBody isActive>
