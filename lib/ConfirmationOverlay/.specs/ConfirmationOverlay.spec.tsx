0f26c63d9637bfccde12b594974d3028595ad8cc
Shane Prendergast <contact@webknit.co.uk>
2023-09-06 14:53:47 +0100

    Update ConfirmationOverlay.spec.tsx

--- lib/ConfirmationOverlay/.specs/ConfirmationOverlay.spec.tsx
+++ lib/ConfirmationOverlay/.specs/ConfirmationOverlay.spec.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 
