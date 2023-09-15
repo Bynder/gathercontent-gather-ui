705cebbf5cf5e332a18296e16158890225fcc25a
TimBryanDev <hi@timbryan.dev>
2023-09-15 14:25:28 +0100

    :wrench: include ts in vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,36 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import magicalSvg from 'vite-plugin-magical-svg'
-import tsconfigPaths from 'vite-tsconfig-paths';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import magicalSvg from "vite-plugin-magical-svg";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   test: {
     globals: true,
-    include: ['**/.specs/**/*.+(spec).tsx'],
+    include: ["**/.specs/**/*.+(spec).ts(x)?"],
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ["text", "json", "html"],
     },
-    environment: 'jsdom',
+    environment: "jsdom",
   },
   plugins: [
     magicalSvg({
-      target: 'react',
+      target: "react",
     }),
     react(),
     tsconfigPaths(),
   ],
   resolve: {
     alias: {
-      "lib": path.resolve(__dirname, "./lib"),
-      "tests": path.resolve(__dirname, "./tests"),
-      "stories": path.resolve(__dirname, "./stories"),
-      "src": path.resolve(__dirname, "./lib/src"),
-      "components": path.resolve(__dirname, "./lib/src/components"),
-      "modules": path.resolve(__dirname, "./lib/src/modules"),
-      "helpers": path.resolve(__dirname, "./lib/src/helpers"),
-      "assets": path.resolve(__dirname, "./assets"),
+      lib: path.resolve(__dirname, "./lib"),
+      tests: path.resolve(__dirname, "./tests"),
+      stories: path.resolve(__dirname, "./stories"),
+      src: path.resolve(__dirname, "./lib/src"),
+      components: path.resolve(__dirname, "./lib/src/components"),
+      modules: path.resolve(__dirname, "./lib/src/modules"),
+      helpers: path.resolve(__dirname, "./lib/src/helpers"),
+      assets: path.resolve(__dirname, "./assets"),
     },
   },
 });
