ea4de9daaf3f92256e5b1dd415776d524be174e9
Shane Prendergast <contact@webknit.co.uk>
2023-10-02 15:10:39 +0100

    adding original logo back in

---
+++ lib/LogoGC/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logoSVG from "../../assets/logo.svg";
+
+/**
+ * @usage
+ *
+ * <Logo url="http://example.com/myimage.png" alt="Alt Tag" />
+ */
+
+export function Logo(props: any) {
+  const LogoPath = props.url || logoSVG;
+  const image =
+    typeof LogoPath === "string" ? (
+      <img src={props.url} alt={props.alt} className="logo__image" />
+    ) : (
+      <span className="logo__image">
+        <LogoPath />
+      </span>
+    );
+  return <span className="logo">{image}</span>;
+}
+
+Logo.defaultProps = {
+  url: "",
+  alt: "GatherContent Logo",
+};
+
+export default Logo;
