843c8dd460d64361992fe245f0819ce8e804a063
micmcgrorty <michaelmcgrorty2@gmail.com>
2023-11-23 10:33:12 +0000

    actually dont change the classnames

--- lib/src/prefabs/modalUpload/ModalUpload.tsx
+++ lib/src/prefabs/modalUpload/ModalUpload.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import { Modal as NewModal } from 'lib';
 import { Modal } from '../../components/modal/Modal';
 import { ModalUploadBody } from './ModalUploadBody';
 
-export function ModalUpload({
-  children,
-  ...rest
-}: any) {
+export function ModalUpload({ children, ...rest }: any) {
   return (
-    <Modal {...rest} size="large">
+    <NewModal.Container {...rest} size="large">
       {children}
-    </Modal>
+    </NewModal.Container>
   );
 }
 
 ModalUpload.Footer = Modal.FooterConfirm;
-ModalUpload.Header = Modal.Header;
+ModalUpload.Header = NewModal.Header;
 ModalUpload.Body = ModalUploadBody;
