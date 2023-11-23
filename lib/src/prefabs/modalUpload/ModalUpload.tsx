e80f412ce023ad0e75344fab7a51a3d1f04c1cf0
micmcgrorty <michaelmcgrorty2@gmail.com>
2023-11-23 10:45:55 +0000

    actually go with classes

--- lib/src/prefabs/modalUpload/ModalUpload.tsx
+++ lib/src/prefabs/modalUpload/ModalUpload.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Modal as NewModal } from 'lib';
 import { Modal } from '../../components/modal/Modal';
 import { ModalUploadBody } from './ModalUploadBody';
 
 export function ModalUpload({ children, ...rest }: any) {
   return (
-    <NewModal.Container {...rest} size="large">
+    <Modal {...rest} size="large">
       {children}
-    </NewModal.Container>
+    </Modal>
   );
 }
 
 ModalUpload.Footer = Modal.FooterConfirm;
-ModalUpload.Header = NewModal.Header;
+ModalUpload.Header = Modal.Header;
 ModalUpload.Body = ModalUploadBody;
